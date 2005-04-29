3899ace0aa62c8138a68719d456b9a4fb385af3a
sven <sven@4126deef-30f4-0310-a2a2-883dd235dc3b>
2005-04-29 22:07:45 +0000

    The class patForms_Datasource_Propel had been unintentionally left in the file patForms/Creator/Definition.php. It has now been stripped of and put into the approriate file patForms/Datasource/Propel.php.

---
+++ contrib/patForms/Datasource/Propel.php
@@ -0,0 +1,62 @@
+<?php
+
+class patForms_Datasource_Propel {
+
+	private $peername;
+	private $label;
+	private $value;
+
+	public function __construct($conf) {
+
+		$this->peername = $conf['peername'];
+		$this->label = $conf['label'];
+		$this->value = $conf['value'];
+	}
+
+	public function getValues() {
+
+		$map = call_user_func(array($this->peername, 'getPhpNameMap'));
+
+		$c = new Criteria();
+		$c->clearSelectColumns();
+
+		foreach (array($this->label, $this->value) as $arr) {
+			foreach ($arr['members'] as $member) {
+				if (is_array($member)) {
+					foreach ($member as $member) {
+						$c->addSelectColumn(constant($this->peername . '::' . $map[$member]));
+					}
+				} else {
+					$c->addSelectColumn(constant($this->peername . '::' . $map[$member]));
+				}
+			}
+		}
+
+		$rs = AuthorPeer::doSelectRs($c);
+		$rs->setFetchmode(ResultSet::FETCHMODE_ASSOC);
+		while ($rs->next()) {
+			$row = $rs->getRow();
+			foreach (array('label', 'value') as $key) {
+				$arr = $this->$key;
+				$params = array($arr['mask']);
+				foreach ($arr['members'] as $member) {
+					if (is_array($member)) {
+						foreach ($member as $member) {
+							$field_name = strtolower($map[$member]); // TODO is this always true?
+							$params[] = $row[$field_name];
+						}
+					} else {
+						$field_name = strtolower($map[$member]); // TODO is this always true?
+						$params[] = $row[$field_name];
+					}
+				}
+				$$key = call_user_func_array('sprintf', $params);
+				$tmp[$key] = $$key;
+			}
+			$result[] = $tmp;
+		}
+
+		return $result;
+	}
+}
+?>
