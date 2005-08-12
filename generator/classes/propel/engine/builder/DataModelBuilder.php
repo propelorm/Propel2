41f07d69d79a496c6ab41f764e56662ad2189b07
hans <hans@4126deef-30f4-0310-a2a2-883dd235dc3b>
2005-08-12 14:50:15 +0000

    updated phpdoc, re-arranged things a bit.

--- generator/classes/propel/engine/builder/DataModelBuilder.php
+++ generator/classes/propel/engine/builder/DataModelBuilder.php
@@ -27,31 +27,30 @@
  * This could be extended by classes that build SQL DDL, PHP classes, configuration
  * files, input forms, etc.
  * 
+ * This class has a static method to return the correct builder subclass identified by 
+ * a given key.  Note that in order for this factory method to work, the properties have to have
+ * been loaded first.  Usage should look something like this (from within a AbstractProelDataModelTask subclass):
+ * 
+ * <code>
+ * DataModelBuilder::setBuildProperties($this->getPropelProperties());
+ * $builder = DataModelBuilder::builderFactory($table, 'peer');
+ * // $builder (by default) instanceof PHP5ComplexPeerBuilder
+ * </code>
+ * 
  * @author Hans Lellelid <hans@xmpl.org>
  * @package propel.engine.builder
  */
 abstract class DataModelBuilder {
 	
-	/**
-	 * The current table.
-	 * @var Table
-	 */
-	private $table;	
+	// --------------------------------------------------------------
+	// Static properties & methods
+	// --------------------------------------------------------------
 	
 	/**
 	 * Build properties (after they've been transformed from "propel.some.name" => "someName").
 	 * @var array string[]
 	 */
 	private static $buildProperties = array();
-	
-	/**
-	 * Creates new instance of DataModelBuilder subclass.
-	 * @param Table $table The Table which we are using to build [OM, DDL, etc.].
-	 */
-	public function __construct(Table $table)
-	{
-		$this->table = $table;
-	}
 	
 	/**
 	 * Sets the [name transformed] build properties to use.
@@ -73,31 +72,27 @@ abstract class DataModelBuilder {
 	}
 	
 	/**
-	 * 
+	 * Factory method to load a new builder instance based on specified type.
+	 * @param Table $table
+	 * @param $type
+	 * @throws BuildException if specified class cannot be found / loaded.
 	 */
-	protected function getPlatform()
+	public static function builderFactory(Table $table, $type)
 	{
-		return $this->getTable()->getDatabase()->getPlatform();
+		if (empty(self::$buildProperties)) {
+		    throw new BuildException("Cannot call builderFactory() method when no build properties have been loaded (hint: Did you call DataModelBuilder::setBuildProperties(\$props) first?)");
+		}
+		$propname = 'builder' . ucfirst(strtolower($type)) . 'Class';
+		$classpath = self::getBuildProperty($propname);
+		if (empty($classpath)) {
+			throw new BuildException("Unable to find class path for '$propname' property.");
+		}
+		$classname = Phing::import($classpath);
+		return new $classname($table);
 	}
 	
 	/**
-	 * 
-	 */
-	protected function getDatabase()
-	{
-		return $this->getTable()->getDatabase();
-	}
-	
-	/**
-	 * 
-	 */
-	protected function getTable()
-	{
-		return $this->table;
-	}	
-    
-    /**
-     * Gets the path to be used in include()/require() statement.
+     * Utility function to build a path for use in include()/require() statement.
      * 
      * Supports two function signatures:
      * (1) getFilePath($dotPathClass);
@@ -117,22 +112,51 @@ abstract class DataModelBuilder {
             return $path . $extension;
         }
     }
+	
+	// --------------------------------------------------------------
+	// Non-static properties & methods inherited by subclasses
+	// --------------------------------------------------------------
 
 	/**
-	 * Factory method to load a new builder instance based on specified type.
-	 * @param Table $table
-	 * @param $type
-	 * @throws BuildException if specified class cannot be found / loaded.
+	 * The current table.
+	 * @var Table
 	 */
-	public static function builderFactory(Table $table, $type)
+	private $table;	
+
+	/**
+	 * Creates new instance of DataModelBuilder subclass.
+	 * @param Table $table The Table which we are using to build [OM, DDL, etc.].
+	 */
+	public function __construct(Table $table)
 	{
-		$propname = 'builder' . ucfirst(strtolower($type)) . 'Class';
-		$classpath = self::getBuildProperty($propname);
-		if (empty($classpath)) {
-			throw new BuildException("Unable to find class path for '$propname' property.");
-		}
-		$classname = Phing::import($classpath);
-		return new $classname($table);
+		$this->table = $table;
 	}
+	
+	/**
+	 * Returns the Platform class for this table (database).
+	 * @return Platform
+	 */
+	protected function getPlatform()
+	{
+		return $this->getTable()->getDatabase()->getPlatform();
+	}
+	
+	/**
+	 * Returns the database for current table.
+	 * @return Database
+	 */
+	protected function getDatabase()
+	{
+		return $this->getTable()->getDatabase();
+	}
+	
+	/**
+	 * Returns the current Table object.
+	 * @return Table
+	 */
+	protected function getTable()
+	{
+		return $this->table;
+	}	
 	
 }
