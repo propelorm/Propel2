dd9d5a44c1fed123eded6ad381e9d2523d5266a8
hans <hans@4126deef-30f4-0310-a2a2-883dd235dc3b>
2005-10-26 01:42:52 +0000

    Added basic doSelectJoin*() test.

--- generator/test/classes/propel/GeneratedPeerTest.php
+++ generator/test/classes/propel/GeneratedPeerTest.php
@@ -277,5 +277,24 @@ class GeneratedPeerTest extends BookstoreTestBase {
 		$this->assertEquals($limitcount, count($results2), "Expected $limitcount results from BookPeer::doSelectJoinAuthor()");
 
 	}
-
+	
+	/**
+	 * Test the basic functionality of the doSelectJoin*() methods.
+	 */
+	public function testDoSelectJoin()
+	{
+		$c = new Criteria();
+		$joinBooks = BookPeer::doSelectJoinAuthor($c);
+		
+		$obj = $joinBooks[0];
+		$joinSize = strlen(serialize($obj));
+		
+		$books = BookPeer::doSelect($c);
+		$obj = $books[0];
+		$size = strlen(serialize($obj));
+		
+		$this->assertEquals(count($books), count($joinBooks), "Expected to find same number of rows in doSelectJoin*() call as doSelect() call.");
+		$this->assertTrue($joinSize > $size, "Expected a serialized join object to be larger than a non-join object.");
+	}
+	
 }
