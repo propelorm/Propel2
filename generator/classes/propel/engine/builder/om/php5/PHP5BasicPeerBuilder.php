e191f5dc056bee4c9dca69e629512c11aa257100
hans <hans@4126deef-30f4-0310-a2a2-883dd235dc3b>
2007-03-02 21:43:42 +0000

    * Applying fix for #309 to 1.3 branch.  * Adding propel.useDateTimeClass=true to bookstore build.properties

--- generator/classes/propel/engine/builder/om/php5/PHP5BasicPeerBuilder.php
+++ generator/classes/propel/engine/builder/om/php5/PHP5BasicPeerBuilder.php
@@ -374,10 +374,16 @@ Propel::getDatabaseMap(".$this->getClassname()."::DATABASE_NAME)->addTableBuilde
 					$script .= "
 	/** A key representing a particular subclass */
 	const CLASSKEY_".strtoupper($child->getKey())." = '" . $child->getKey() . "';
-
+";
+					
+	if (strtoupper($child->getClassname()) != strtoupper($child->getKey())) {
+		$script .= "
 	/** A key representing a particular subclass */
 	const CLASSKEY_".strtoupper($child->getClassname())." = '" . $child->getKey() . "';
-
+";
+	}
+	
+	$script .= "
 	/** A class that can be returned by this peer. */
 	const CLASSNAME_".strtoupper($child->getKey())." = '". $childBuilder->getClasspath() . "';
 ";
