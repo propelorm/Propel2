025154aed41c409b7dfdcdb2c16639e02c210327
hans <hans@4126deef-30f4-0310-a2a2-883dd235dc3b>
2007-11-21 23:22:34 +0000

    Refs #519 - Adding unit test.

--- generator/test/classes/propel/GeneratedPeerTest.php
+++ generator/test/classes/propel/GeneratedPeerTest.php
@@ -466,5 +466,81 @@ class GeneratedPeerTest extends BookstoreTestBase {
 			$this->fail("Expected to get no exception when removing an instance from the pool.");
 		}
 	}
+	
+	/**
+	 * @see        testDoDeleteCompositePK()
+	 */
+	private function createBookWithId($id)
+	{
+		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
+		$b = BookPeer::retrieveByPK($id);
+		if (!$b) {
+			$b = new Book();
+			$b->setTitle("Book$id")->setISBN("BookISBN$id")->save();
+			$b1Id = $b->getId();
+			$sql = "UPDATE " . BookPeer::TABLE_NAME . " SET id = ? WHERE id = ?";
+			$stmt = $con->prepare($sql);
+			$stmt->bindValue(1, $id);
+			$stmt->bindValue(2, $b1Id);
+			$stmt->execute();
+		}
+	}
+	
+	/**
+	 * @see        testDoDeleteCompositePK()
+	 */
+	private function createReaderWithId($id)
+	{
+		$con = Propel::getConnection(BookReaderPeer::DATABASE_NAME);
+		$r = BookReaderPeer::retrieveByPK($id);
+		if (!$r) {
+			$r = new BookReader();
+			$r->setName('Reader'.$id)->save();
+			$r1Id = $r->getId();
+			$sql = "UPDATE " . BookReaderPeer::TABLE_NAME . " SET id = ? WHERE id = ?";
+			$stmt = $con->prepare($sql);
+			$stmt->bindValue(1, $id);
+			$stmt->bindValue(2, $r1Id);
+			$stmt->execute();
+		}
+	}
+	
+	/**
+	 * @link       http://propel.phpdb.org/trac/ticket/519
+	 */
+	public function testDoDeleteCompositePK()
+	{
+		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
+		
+		ReaderFavoritePeer::doDeleteAll();
+		// Create book and reader with ID 1
+		// Create book and reader with ID 2
+		
+		$this->createBookWithId(1);
+		$this->createBookWithId(2);
+		$this->createReaderWithId(1);
+		$this->createReaderWithId(2);
+		
+		for($i=1; $i <= 2; $i++) {
+			for($j=1; $j <= 2; $j++) {
+				$rf = new ReaderFavorite();
+				$rf->setBookId($i);
+				$rf->setReaderId($j);
+				$rf->save();
+			}
+		}
+
+		$this->assertEquals(4, ReaderFavoritePeer::doCount(new Criteria()));
+		
+		// Now delete 2 of those rows 
+		ReaderFavoritePeer::doDelete(array(array(1,2), array(2,2)));
+		
+		$this->assertEquals(2, ReaderFavoritePeer::doCount(new Criteria()));
+		
+		$this->assertNotNull(ReaderFavoritePeer::retrieveByPK(2,1));
+		$this->assertNotNull(ReaderFavoritePeer::retrieveByPK(1,1));
+		$this->assertNull(ReaderFavoritePeer::retrieveByPK(1,2));
+		$this->assertNull(ReaderFavoritePeer::retrieveByPK(2,2));
+	}
 
 }
