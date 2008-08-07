69bee00e6d2908a56db1cbabd05bf2750a57bdd5
soenke <soenke@4126deef-30f4-0310-a2a2-883dd235dc3b>
2008-08-07 00:18:08 +0000

    fixes #668

--- runtime/classes/propel/map/TableMap.php
+++ runtime/classes/propel/map/TableMap.php
@@ -330,7 +330,7 @@ class TableMap {
 		$col = new ColumnMap($name, $this);
 
 		if ($fkTable && $fkColumn) {
-			if (substr($fkColumn, '.') > 0 && substr($fkColumn, $fkTable) !== false) {
+			if (substr($fkColumn, '.') > 0 && strpos($fkColumn, $fkTable) !== false) {
 				$fkColumn = substr($fkColumn, strlen($fkTable) + 1);
 			}
 			$col->setForeignKey($fkTable, $fkColumn);
