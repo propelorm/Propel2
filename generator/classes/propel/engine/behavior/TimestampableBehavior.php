e6d0741249479656bc6425e2f5c827b304a8633e
Francois Zaninotto <fzaninotto@gmail.com>
2009-10-31 12:48:19 +0000

    [1.4][1.5] Fixed missing PHPDoc

--- generator/classes/propel/engine/behavior/TimestampableBehavior.php
+++ generator/classes/propel/engine/behavior/TimestampableBehavior.php
@@ -1,5 +1,33 @@
 <?php
 
+/*
+ *  $Id: TimestampableBehavior.php 1262 2009-10-26 20:54:39Z francois $
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information please see
+ * <http://propel.phpdb.org>.
+ */
+ 
+/**
+ * Gives a model class the ability to track creation and last modification dates
+ * Uses two additional columns storing the creation and update date
+ *
+ * @author     François Zaninotto
+ * @version    $Revision: 1066 $
+ * @package    propel.engine.behavior
+ */
 class TimestampableBehavior extends Behavior
 {
   // default parameters value
