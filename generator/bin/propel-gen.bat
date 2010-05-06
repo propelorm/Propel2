48c50a322f3f6be5a2077a7fdde3386a5b4d96b0
Francois Zaninotto <fzaninotto@gmail.com>
2010-05-06 14:49:25 +0000

    [1.5] Fixed propel-gen executable on windows (closes #942)

--- generator/bin/propel-gen.bat
+++ generator/bin/propel-gen.bat
@@ -8,7 +8,7 @@ rem *********************************************************************
 rem This script will do the following:
 rem - check for PHING_COMMAND env, if found, use it.
 rem   - if not found detect php, if found use it, otherwise err and terminate
-rem - check for PROPEL_GEN_HOME evn, if found use it
+rem - check for PROPEL_GEN_HOME env, if found use it
 rem   - if not found error and leave
 
 if "%OS%"=="Windows_NT" @setlocal
@@ -16,39 +16,17 @@ if "%OS%"=="Windows_NT" @setlocal
 rem %~dp0 is expanded pathname of the current script under NT
 set DEFAULT_PROPEL_GEN_HOME=%~dp0..
 
-goto init
-goto cleanup
-
-:init
-
 if "%PROPEL_GEN_HOME%" == "" set PROPEL_GEN_HOME=%DEFAULT_PROPEL_GEN_HOME%
 set DEFAULT_PROPEL_GEN_HOME=
 
-if "%PHING_COMMAND%" == "" goto no_phingcommand
-
-goto run
-goto cleanup
-
-:run
+if "%PHING_COMMAND%" == "" set PHING_COMMAND=phing.bat
 
 set nbArgs=0
 for %%x in (%*) do Set /A nbArgs+=1
-if %nbArgs%==1 (
+if %nbArgs% leq 1 (
   %PHING_COMMAND% -f "%PROPEL_GEN_HOME%\build.xml" -Dusing.propel-gen=true -Dproject.dir="%CD%" %*
 ) else (
   %PHING_COMMAND% -f "%PROPEL_GEN_HOME%\build.xml" -Dusing.propel-gen=true -Dproject.dir=%*
 )
-goto cleanup
 
-:no_phingcommand
-REM echo ------------------------------------------------------------------------
-REM echo WARNING: Set environment var PHING_COMMAND to the location of your phing
-REM echo          executable (e.g. C:\PHP\phing.bat). 
-REM echo Proceeding with assumption that phing.bat is on Path
-REM echo ------------------------------------------------------------------------
-set PHING_COMMAND=phing.bat
-goto init
-
-:cleanup
 if "%OS%"=="Windows_NT" @endlocal
-REM pause
--- generator/pear/pear-propel-gen.bat
+++ generator/pear/pear-propel-gen.bat
@@ -9,7 +9,7 @@
 :: Please set following to the "phing" script.  By default this is expected to be
 :: on your path.  (You don't need to modify this file if that is the case.)
   
-  SET phingScript=phing
+SET phingScript=phing
 
 ::---------------------------------------------------------------------------------
 ::---------------------------------------------------------------------------------
@@ -19,7 +19,7 @@
 
 set nbArgs=0
 for %%x in (%*) do Set /A nbArgs+=1
-if %nbArgs%==1 (
+if %nbArgs% leq 1 (
   "%phingScript%" -f "@DATA-DIR@\propel_generator\pear-build.xml" -Dproject.dir="%CD%" %*
 ) else (
   "%phingScript%" -f "@DATA-DIR@\propel_generator\pear-build.xml" -Dproject.dir=%*
