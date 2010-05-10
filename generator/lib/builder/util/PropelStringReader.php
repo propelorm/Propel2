dbcb5efd4aed4eac432ab36d9cb7b19dad265621
Francois Zaninotto <fzaninotto@gmail.com>
2010-05-10 20:18:05 +0000

    [1.5] Fixed warning with new StringReader

--- generator/lib/builder/util/PropelStringReader.php
+++ generator/lib/builder/util/PropelStringReader.php
@@ -4,6 +4,16 @@ include_once 'phing/system/io/StringReader.php';
 
 class PropelStringReader extends StringReader
 {
+	/**
+	 * @var string
+	 */
+	protected $_string;
+    
+	/**
+	 * @var int
+	 */
+	protected $currPos = 0;
+
 	public function eof()
 	{
 		return $this->currPos == strlen($this->_string);
