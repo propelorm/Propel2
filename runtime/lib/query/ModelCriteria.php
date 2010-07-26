5f861d0e71bca9800a0e41ab6ec0888701f2b9eb
Francois Zaninotto <fzaninotto@gmail.com>
2010-07-26 11:07:02 +0000

    [1.5][1.6] Fixed coding standards in `ModelCriteria::select()` (refs #991)

--- runtime/lib/query/ModelCriteria.php
+++ runtime/lib/query/ModelCriteria.php
@@ -445,72 +445,75 @@ class ModelCriteria extends Criteria
 		return $this;
 	}
 	
-  /**
-   * Makes the ModelCriteria return a string, array, or PropelArrayCollection
-   * Examples:
-   *   ArticleQuery::create()->select('Name')->find();
-   *   => PropelArrayCollection Object ('Foo', 'Bar')
-   *
-   *   ArticleQuery::create()->select('Name')->findOne();
-   *   => string 'Foo'
-   *
-   *   ArticleQuery::create()->select(array('Id', 'Name'))->find();
-   *   => PropelArrayCollection Object (
-   *        array('Id' => 1, 'Name' => 'Foo'),
-   *        array('Id' => 2, 'Name' => 'Bar')
-   *      )
-   *
-   *   ArticleQuery::create()->select(array('Id', 'Name'))->findOne();
-   *   => array('Id' => 1, 'Name' => 'Foo')
-   *
-   * @param       mixed $columnArray A list of column names (e.g. array('Title', 'Category.Name', 'c.Content')) or a single column name (e.g. 'Name')
-   *
-   * @return     ModelCriteria The current object, for fluid interface
-   */
-  public function select($columnArray)
-  {
-    if (! count($columnArray) || $columnArray == '') {
-      throw new PropelException ( 'You must ask for at least one column' );
-    }
-
-    if ($columnArray == '*') {
-      $columnArray = array ();
-      foreach ( call_user_func ( array ($this->modelPeerName, 'getFieldNames' ), BasePeer::TYPE_PHPNAME ) as $column ) {
-        $columnArray [] = $this->modelName . '.' . $column;
-      }
-    }
-
-    $this->select = $columnArray;
-
-    return $this;
-  }
-  
-  protected function configureSelectColumns()
-  {
-      if (! is_null ( $this->select )) {
-      // select() needs the PropelSimpleArrayFormatter
-      $this->setFormatter('PropelSimpleArrayFormatter');
-
-      // clear only the selectColumns, clearSelectColumns() clears asColumns too
-      $this->selectColumns = array ();
-
-      // We need to set the primary table name, since in the case that there are no WHERE columns
-      // it will be impossible for the BasePeer::createSelectSql() method to determine which
-      // tables go into the FROM clause.
-      $this->setPrimaryTableName ( constant ( $this->modelPeerName . '::TABLE_NAME' ) );
-
-      // Add requested columns which are not withColumns
-      $columnNames = is_array ( $this->select ) ? $this->select : array ($this->select );
-      foreach ( $columnNames as $columnName ) {
-        // check if the column was added by a withColumn, if not add it
-        if (! array_key_exists ( $columnName, $this->getAsColumns () )) {
-          $column = $this->getColumnFromName ( $columnName );
-          // always put quotes around the columnName to be safe, we strip them in the formatter
-          $this->addAsColumn ( '"' . $columnName . '"', $column [1] );
-        }
-      }
-    }
-  }
+	/**
+	* Makes the ModelCriteria return a string, array, or PropelArrayCollection
+	* Examples:
+	*   ArticleQuery::create()->select('Name')->find();
+	*   => PropelArrayCollection Object ('Foo', 'Bar')
+	*
+	*   ArticleQuery::create()->select('Name')->findOne();
+	*   => string 'Foo'
+	*
+	*   ArticleQuery::create()->select(array('Id', 'Name'))->find();
+	*   => PropelArrayCollection Object (
+	*        array('Id' => 1, 'Name' => 'Foo'),
+	*        array('Id' => 2, 'Name' => 'Bar')
+	*      )
+	*
+	*   ArticleQuery::create()->select(array('Id', 'Name'))->findOne();
+	*   => array('Id' => 1, 'Name' => 'Foo')
+	*
+	* @param       mixed $columnArray A list of column names (e.g. array('Title', 'Category.Name', 'c.Content')) or a single column name (e.g. 'Name')
+	*
+	* @return     ModelCriteria The current object, for fluid interface
+	*/
+	public function select($columnArray)
+	{
+		if (!count($columnArray) || $columnArray == '') {
+			throw new PropelException('You must ask for at least one column');
+		}
+		
+		if ($columnArray == '*') {
+			$columnArray = array();
+			foreach (call_user_func(array($this->modelPeerName, 'getFieldNames'), BasePeer::TYPE_PHPNAME) as $column) {
+				$columnArray []= $this->modelName . '.' . $column;
+			}
+		}
+		
+		$this->select = $columnArray;
+		
+		return $this;
+	}
+	
+	protected function configureSelectColumns()
+	{
+		if (is_null($this->select)) {
+			// leave early
+			return;
+		}
+		
+		// select() needs the PropelSimpleArrayFormatter
+		$this->setFormatter('PropelSimpleArrayFormatter');
+		
+		// clear only the selectColumns, clearSelectColumns() clears asColumns too
+		$this->selectColumns = array();
+		
+		// We need to set the primary table name, since in the case that there are no WHERE columns
+		// it will be impossible for the BasePeer::createSelectSql() method to determine which
+		// tables go into the FROM clause.
+		$this->setPrimaryTableName(constant($this->modelPeerName . '::TABLE_NAME'));
+		
+		// Add requested columns which are not withColumns
+		$columnNames = is_array($this->select) ? $this->select : array($this->select);
+		foreach ($columnNames as $columnName) {
+			// check if the column was added by a withColumn, if not add it
+			if (!array_key_exists($columnName, $this->getAsColumns())) {
+				$column = $this->getColumnFromName ($columnName);
+				// always put quotes around the columnName to be safe, we strip them in the formatter
+				$this->addAsColumn('"' . $columnName . '"', $column[1]);
+			}
+		}
+	}
 	
 	/**
 	 * This method returns the previousJoin for this ModelCriteria,
