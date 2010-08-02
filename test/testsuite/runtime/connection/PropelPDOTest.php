8361d0cffaff324cdfe380d8dbb8b826f04fbd38
KRavEN <KRavEN@4126deef-30f4-0310-a2a2-883dd235dc3b>
2010-08-02 18:05:08 +0000

    [1.5][1.6] Added tests for PDOStatement::fetch after PDO::commit PDO driver support

--- test/testsuite/runtime/connection/PropelPDOTest.php
+++ test/testsuite/runtime/connection/PropelPDOTest.php
@@ -28,29 +28,77 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 		$con->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
 		$this->assertEquals(PDO::CASE_LOWER, $con->getAttribute(PDO::ATTR_CASE));
 	}
-	
+
+public function testCommitBeforeFetch()
+	{
+		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
+		$a = new Author();
+		$a->setFirstName('Test');
+		$a->setLastName('User');
+		$a->save($con);
+
+		$con->beginTransaction();
+		$stmt = $con->prepare('SELECT author.FIRST_NAME, author.LAST_NAME FROM author');
+
+		$stmt->execute();
+		$con->commit();
+		$authorArr = array(0 => 'Test', 1 => 'User');
+
+		$i = 0;
+		try {
+			$row = $stmt->fetch( PDO::FETCH_NUM );
+			$stmt->closeCursor();
+			$this->assertEquals($authorArr, $row, 'PDO driver supports calling $stmt->fetch after the transaction has been closed');
+		} catch (PDOException $e) {
+			$this->fail("PDO driver does not support calling \$stmt->fetch after the transaction has been closed.\nFails with error ".$e->getMessage());
+		}
+		AuthorPeer::doDeleteAll($con);
+	}
+
+	public function testCommitAfterFetch()
+	{
+		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
+		$a = new Author();
+		$a->setFirstName('Test');
+		$a->setLastName('User');
+		$a->save($con);
+
+		$con->beginTransaction();
+		$stmt = $con->prepare('SELECT author.FIRST_NAME, author.LAST_NAME FROM author');
+
+		$stmt->execute();
+		$authorArr = array(0 => 'Test', 1 => 'User');
+
+		$i = 0;
+		$row = $stmt->fetch( PDO::FETCH_NUM );
+		$stmt->closeCursor();
+		$con->commit();
+		$this->assertEquals($authorArr, $row, 'PDO driver supports calling $stmt->fetch before the transaction has been closed');
+		AuthorPeer::doDeleteAll($con);
+	}
+
 	public function testNestedTransactionCommit()
 	{
 		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
 		$driver = $con->getAttribute(PDO::ATTR_DRIVER_NAME);
-		
+
 		$this->assertEquals(0, $con->getNestedTransactionCount(), 'nested transaction is equal to 0 before transaction');
 		$this->assertFalse($con->isInTransaction(), 'PropelPDO is not in transaction by default');
-		
+
 		$con->beginTransaction();
-		
+
 		$this->assertEquals(1, $con->getNestedTransactionCount(), 'nested transaction is incremented after main transaction begin');
 		$this->assertTrue($con->isInTransaction(), 'PropelPDO is in transaction after main transaction begin');
-		
+
 		try {
-			
+
 			$a = new Author();
 			$a->setFirstName('Test');
 			$a->setLastName('User');
 			$a->save($con);
 			$authorId = $a->getId();
 			$this->assertNotNull($authorId, "Expected valid new author ID");
-			
+
 			$con->beginTransaction();
 
 			$this->assertEquals(2, $con->getNestedTransactionCount(), 'nested transaction is incremented after nested transaction begin');
@@ -64,9 +112,9 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 				$a2->save($con);
 				$authorId2 = $a2->getId();
 				$this->assertNotNull($authorId2, "Expected valid new author ID");
-				
+
 				$con->commit();
-				
+
 				$this->assertEquals(1, $con->getNestedTransactionCount(), 'nested transaction decremented after nested transaction commit');
 				$this->assertTrue($con->isInTransaction(), 'PropelPDO is in transaction after main transaction commit');
 
@@ -74,16 +122,16 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 				$con->rollBack();
 				throw $e;
 			}
-			
+
 			$con->commit();
-			
+
 			$this->assertEquals(0, $con->getNestedTransactionCount(), 'nested transaction decremented after main transaction commit');
 			$this->assertFalse($con->isInTransaction(), 'PropelPDO is not in transaction after main transaction commit');
 
 		} catch (Exception $e) {
 			$con->rollBack();
 		}
-		
+
 		AuthorPeer::clearInstancePool();
 		$at = AuthorPeer::retrieveByPK($authorId);
 		$this->assertNotNull($at, "Committed transaction is persisted in database");
@@ -98,18 +146,18 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 	{
 		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
 		$driver = $con->getAttribute(PDO::ATTR_DRIVER_NAME);
-		
+
 		$con->beginTransaction();
 		try {
-			
+
 			$a = new Author();
 			$a->setFirstName('Test');
 			$a->setLastName('User');
 			$a->save($con);
 			$authorId = $a->getId();
-			
+
 			$this->assertNotNull($authorId, "Expected valid new author ID");
-			
+
 			$con->beginTransaction();
 
 			$this->assertEquals(2, $con->getNestedTransactionCount(), 'nested transaction is incremented after nested transaction begin');
@@ -126,17 +174,17 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 
 				throw $x;
 			}
-			
+
 			$con->commit();
 		} catch (Exception $x) {
 			$con->rollBack();
 		}
-		
+
 		AuthorPeer::clearInstancePool();
 		$at = AuthorPeer::retrieveByPK($authorId);
 		$this->assertNull($at, "Rolled back transaction is not persisted in database");
 	}
-	
+
 	/**
 	 * @link       http://propel.phpdb.org/trac/ticket/699
 	 */
@@ -144,18 +192,18 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 	{
 		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
 		$driver = $con->getAttribute(PDO::ATTR_DRIVER_NAME);
-		
+
 		$con->beginTransaction();
 		try {
-			
+
 			$a = new Author();
 			$a->setFirstName('Test');
 			$a->setLastName('User');
 			$a->save($con);
-			
-			$authorId = $a->getId();	
+
+			$authorId = $a->getId();
 			$this->assertNotNull($authorId, "Expected valid new author ID");
-			
+
 			$con->beginTransaction();
 			try {
 
@@ -172,25 +220,25 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 				$con->rollBack();
 				// NO RETHROW
 			}
-			
+
 			$a3 = new Author();
 			$a3->setFirstName('Test2');
 			$a3->setLastName('User2');
 			$a3->save($con);
-			
+
 			$authorId3 = $a3->getId();
 			$this->assertNotNull($authorId3, "Expected valid new author ID");
-			 
+
 			$con->commit();
 			$this->fail("Commit fails after a nested rollback");
 		} catch (PropelException $e) {
 			$this->assertTrue(true, "Commit fails after a nested rollback");
 			$con->rollback();
 		}
-		
+
 		AuthorPeer::clearInstancePool();
 		$at = AuthorPeer::retrieveByPK($authorId);
-		$this->assertNull($at, "Rolled back transaction is not persisted in database");		
+		$this->assertNull($at, "Rolled back transaction is not persisted in database");
 		$at2 = AuthorPeer::retrieveByPK($authorId2);
 		$this->assertNull($at2, "Rolled back transaction is not persisted in database");
 		$at3 = AuthorPeer::retrieveByPK($authorId3);
@@ -201,16 +249,16 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 	{
 		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
 		$driver = $con->getAttribute(PDO::ATTR_DRIVER_NAME);
-		
-		// main transaction		
+
+		// main transaction
 		$con->beginTransaction();
-			
+
 		$a = new Author();
 		$a->setFirstName('Test');
 		$a->setLastName('User');
 		$a->save($con);
 		$authorId = $a->getId();
-		
+
 		// nested transaction
 		$con->beginTransaction();
 
@@ -219,27 +267,27 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 		$a2->setLastName('User2');
 		$a2->save($con);
 		$authorId2 = $a2->getId();
-		
+
 		// force rollback
 		$con->forceRollback();
-		
+
 		$this->assertEquals(0, $con->getNestedTransactionCount(), 'nested transaction is null after nested transaction forced rollback');
 		$this->assertFalse($con->isInTransaction(), 'PropelPDO is not in transaction after nested transaction force rollback');
-		
+
 		AuthorPeer::clearInstancePool();
 		$at = AuthorPeer::retrieveByPK($authorId);
-		$this->assertNull($at, "Rolled back transaction is not persisted in database");		
+		$this->assertNull($at, "Rolled back transaction is not persisted in database");
 		$at2 = AuthorPeer::retrieveByPK($authorId2);
 		$this->assertNull($at2, "Forced Rolled back nested transaction is not persisted in database");
 	}
-	
+
 	public function testLatestQuery()
 	{
 		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
 		$con->setLastExecutedQuery(123);
 		$this->assertEquals(123, $con->getLastExecutedQuery(), 'PropelPDO has getter and setter for last executed query');
 	}
-	
+
 	public function testLatestQueryMoreThanTenArgs()
 	{
 		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
@@ -249,7 +297,7 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 		$expected = "SELECT book.ID, book.TITLE, book.ISBN, book.PRICE, book.PUBLISHER_ID, book.AUTHOR_ID FROM `book` WHERE book.ID IN (1,1,1,1,1,1,1,1,1,1,1,1)";
 		$this->assertEquals($expected, $con->getLastExecutedQuery(), 'PropelPDO correctly replaces arguments in queries');
 	}
-	
+
 	public function testQueryCount()
 	{
 		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
@@ -266,7 +314,7 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 		$con->useDebug(true);
 		$this->assertEquals(array('DebugPDOStatement', array($con)), $con->getAttribute(PDO::ATTR_STATEMENT_CLASS), 'statement is DebugPDOStament when debug is true');
 	}
-	
+
 	public function testDebugLatestQuery()
 	{
 		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
@@ -286,11 +334,11 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 			$latestExecutedQuery = str_replace('`', '', $latestExecutedQuery);
 		}
 		$this->assertEquals($latestExecutedQuery, $con->getLastExecutedQuery(), 'PropelPDO updates the last executed query when useLogging is true');
-		
+
 		BookPeer::doDeleteAll($con);
 		$latestExecutedQuery = "DELETE FROM `book`";
 		$this->assertEquals($latestExecutedQuery, $con->getLastExecutedQuery(), 'PropelPDO updates the last executed query on delete operations');
-		
+
 		$sql = 'DELETE FROM book WHERE 1=1';
 		$con->exec($sql);
 		$this->assertEquals($sql, $con->getLastExecutedQuery(), 'PropelPDO updates the last executed query on exec operations');
@@ -325,10 +373,10 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 		$con->useDebug(true);
 		$books = BookPeer::doSelect($c, $con);
 		$this->assertEquals(1, $con->getQueryCount(), 'PropelPDO updates the query count when useLogging is true');
-		
+
 		BookPeer::doDeleteAll($con);
 		$this->assertEquals(2, $con->getQueryCount(), 'PropelPDO updates the query count on delete operations');
-		
+
 		$sql = 'DELETE FROM book WHERE 1=1';
 		$con->exec($sql);
 		$this->assertEquals(3, $con->getQueryCount(), 'PropelPDO updates the query count on exec operations');
@@ -352,24 +400,24 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 	{
 		$con = Propel::getConnection(BookPeer::DATABASE_NAME);
 		$config = Propel::getConfiguration(PropelConfiguration::TYPE_OBJECT);
-		
+
 		// save data to return to normal state after test
 		$logger = $con->getLogger();
-				
+
 		$testLog = new myLogger();
 		$con->setLogger($testLog);
-		
+
 		$logEverything = array('PropelPDO::exec', 'PropelPDO::query', 'PropelPDO::beginTransaction', 'PropelPDO::commit', 'PropelPDO::rollBack', 'DebugPDOStatement::execute');
 		Propel::getConfiguration(PropelConfiguration::TYPE_OBJECT)->setParameter("debugpdo.logging.methods", $logEverything);
 		$con->useDebug(true);
-		
+
 		// test transaction log
 		$con->beginTransaction();
 		$this->assertEquals('log: Begin transaction', $testLog->latestMessage, 'PropelPDO logs begin transation in debug mode');
-		
+
 		$con->commit();
 		$this->assertEquals('log: Commit transaction', $testLog->latestMessage, 'PropelPDO logs commit transation in debug mode');
-		
+
 		$con->beginTransaction();
 		$con->rollBack();
 		$this->assertEquals('log: Rollback transaction', $testLog->latestMessage, 'PropelPDO logs rollback transation in debug mode');
@@ -384,10 +432,10 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 		$con->rollBack();
 		$this->assertEquals('', $testLog->latestMessage, 'PropelPDO does not log nested rollback transation in debug mode');
 		$con->rollback();
-		
+
 		// test query log
 		$con->beginTransaction();
-		
+
 		$c = new Criteria();
 		$c->add(BookPeer::TITLE, 'Harry%s', Criteria::LIKE);
 
@@ -398,14 +446,14 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 		BookPeer::doDeleteAll($con);
 		$latestExecutedQuery = "DELETE FROM `book`";
 		$this->assertEquals('log: ' . $latestExecutedQuery, $testLog->latestMessage, 'PropelPDO logs deletion queries in debug mode');
-		
+
 		$latestExecutedQuery = 'DELETE FROM book WHERE 1=1';
 		$con->exec($latestExecutedQuery);
 		$this->assertEquals('log: ' . $latestExecutedQuery, $testLog->latestMessage, 'PropelPDO logs exec queries in debug mode');
-		
+
 		$con->commit();
-		
-		// return to normal state after test		
+
+		// return to normal state after test
 		$con->setLogger($logger);
 		$config->setParameter("debugpdo.logging.methods", array('PropelPDO::exec', 'PropelPDO::query', 'DebugPDOStatement::execute'));
 	}
@@ -414,9 +462,9 @@ class PropelPDOTest extends PHPUnit_Framework_TestCase
 class myLogger
 {
 	public $latestMessage = '';
-	
+
 	public function __call($method, $arguments)
 	{
-		$this->latestMessage = $method . ': ' . array_shift($arguments); 
+		$this->latestMessage = $method . ': ' . array_shift($arguments);
 	}
 }
