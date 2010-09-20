cb463910b194c0209dcae37bada9d753394b2b6c
Francois Zaninotto <fzaninotto@gmail.com>
2010-09-20 21:44:00 +0000

    [1.6] Speed up tests a little by executing fixtures only when necessary

--- test/testsuite/generator/builder/om/OMBuilderTest.php
+++ test/testsuite/generator/builder/om/OMBuilderTest.php
@@ -10,8 +10,6 @@
 
 require_once dirname(__FILE__) . '/../../../../tools/helpers/bookstore/BookstoreTestBase.php';
 require_once dirname(__FILE__) . '/../../../../../generator/lib/builder/om/OMBuilder.php';
-require_once dirname(__FILE__) . '/../../../../../generator/lib/builder/util/XmlToAppData.php';
-require_once dirname(__FILE__) . '/../../../../../generator/lib/platform/MysqlPlatform.php';
 
 /**
  * Test class for OMBuilder.
@@ -22,43 +20,6 @@ require_once dirname(__FILE__) . '/../../../../../generator/lib/platform/MysqlPl
  */
 class OMBuilderTest extends PHPUnit_Framework_TestCase 
 {
-	public function setUp()
-	{
-		$xmlToAppData = new XmlToAppData(new MysqlPlatform(), "defaultpackage", null);
-		$appData = $xmlToAppData->parseFile(realpath(dirname(__FILE__) . '/../../../../fixtures/bookstore/schema.xml'));
-		$this->database = $appData->getDatabase("bookstore");
-	}
-	
-	protected function getForeignKey($tableName, $index)
-	{
-		$fks = $this->database->getTable($tableName)->getForeignKeys();
-		return $fks[$index];
-	}
-	
-	public static function getRelatedBySuffixDataProvider()
-	{
-		return array(
-			array('book', 0, '', ''),
-			array('essay', 0, 'RelatedByFirstAuthor', 'RelatedByFirstAuthor'),
-			array('essay', 1, 'RelatedBySecondAuthor', 'RelatedBySecondAuthor'),
-			array('essay', 2, 'RelatedById', 'RelatedByNextEssayId'),
-			array('bookstore_employee', 0, 'RelatedById', 'RelatedBySupervisorId'),
-			array('composite_essay', 0, 'RelatedById0', 'RelatedByFirstEssayId'),
-			array('composite_essay', 1, 'RelatedById1', 'RelatedBySecondEssayId'),
-			array('man', 0, 'RelatedByWifeId', 'RelatedByWifeId'),
-			array('woman', 0, 'RelatedByHusbandId', 'RelatedByHusbandId'),
-		);
-	}
-	
-	/**
-	 * @dataProvider getRelatedBySuffixDataProvider
-	 */
-	public function testGetRelatedBySuffix($table, $index, $expectedSuffix, $expectedReverseSuffix)
-	{
-		$fk = $this->getForeignKey($table, $index);
-		$this->assertEquals($expectedSuffix, TestableOMBuilder::getRefRelatedBySuffix($fk));
-		$this->assertEquals($expectedReverseSuffix, TestableOMBuilder::getRelatedBySuffix($fk));
-	}
 
 	public function testClear()
 	{
@@ -100,19 +61,4 @@ EOF;
 EOF;
 		$this->assertEquals($expected, (string) $publisher, 'generated __toString() uses default string format and exportTo()');
 	}
-}
-
-class TestableOMBuilder extends OMBuilder
-{
-	public static function getRelatedBySuffix(ForeignKey $fk)
-	{
-		return parent::getRelatedBySuffix($fk);
-	}
-	
-	public static function getRefRelatedBySuffix(ForeignKey $fk)
-	{
-		return parent::getRefRelatedBySuffix($fk);
-	}
-	
-	public function getUnprefixedClassname() {}
 }
