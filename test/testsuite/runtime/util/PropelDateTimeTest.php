3e13b2938a6f87d0a6f9d3b7d5c730f3403166ed
William DURAND <william.durand1@gmail.com>
2011-08-10 13:46:48 +0200

    Fixed tests for PropelDateTime::newInstance

--- test/testsuite/runtime/util/PropelDateTimeTest.php
+++ test/testsuite/runtime/util/PropelDateTimeTest.php
@@ -134,16 +134,29 @@ class PropelDateTimeTest extends PHPUnit_Framework_TestCase
 		$pdt = unserialize($ser);
 		$this->assertDatesNotEqual($dt, $pdt);
 	}
-	
+
 	/**
 	 * @dataProvider provideValidNewInstanceValues
 	 */
 	public function testNewInstance($value, $expected)
 	{
+        date_default_timezone_set('UTC');
+
 		$dt = PropelDateTime::newInstance($value);
 		$this->assertEquals($expected, $dt->format('Y-m-d H:i:s'));
 	}
-	
+
+	/**
+	 * @dataProvider provideValidNewInstanceValuesGmt1
+	 */
+	public function testNewInstanceGmt1($value, $expected)
+	{
+        date_default_timezone_set('Europe/Paris');
+
+		$dt = PropelDateTime::newInstance($value);
+		$this->assertEquals($expected, $dt->format('Y-m-d H:i:s'));
+	}
+
 	/**
 	 * @expectedException PropelException
 	 */
@@ -151,17 +164,29 @@ class PropelDateTimeTest extends PHPUnit_Framework_TestCase
 	{
 		$dt = PropelDateTime::newInstance('some string');
 	}
-	
+
 	public function provideValidNewInstanceValues()
 	{
 		return array(
-			'Y-m-d' => array('2011-08-10', '2011-08-10 00:00:00'),
-			'unix_timestamp' => array('1312960848', '2011-08-10 10:20:48'),
-			'Y-m-d H:is' => array('2011-08-10 10:22:15', '2011-08-10 10:22:15'),
-			'Ymd' => array('20110810', '2011-08-10 00:00:00'),
+			'Y-m-d'           => array('2011-08-10', '2011-08-10 00:00:00'),
+			// 1312960848 : Wed, 10 Aug 2011 07:20:48 GMT
+			'unix_timestamp'  => array('1312960848', '2011-08-10 07:20:48'),
+			'Y-m-d H:is'      => array('2011-08-10 10:22:15', '2011-08-10 10:22:15'),
+			'Ymd'             => array('20110810', '2011-08-10 00:00:00'),
+			'Ymd'             => array('201107201343', '2011-07-20 13:43:00'),
 			'datetime_object' => array(new DateTime('2011-08-10 10:23:10'), '2011-08-10 10:23:10')
 		);
 	}
 
-
+	public function provideValidNewInstanceValuesGmt1()
+	{
+		return array(
+			// "1312960848" : Wed, 10 Aug 2011 07:20:48 GMT
+			// "2011-08-10 09:20:48" : GMT+1 DST (= GMT +2)
+			'unix_timestamp'  => array('1312960848', '2011-08-10 09:20:48'),
+			// "1323517115" : Sat, 10 Dec 2011 11:38:35 GMT
+			// "2011-12-10 12:38:35" : GMT +1
+			'unix_timestamp'  => array('1323517115', '2011-12-10 12:38:35'),
+		);
+	}
 }
