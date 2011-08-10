cd547f23d5f95c7a794ab2a26c328e3604a6ee0e
William DURAND <william.durand1@gmail.com>
2011-08-10 22:56:19 +0200

    MySQL Driver possibly subject to sql injections with PDO::quote()
    Patch posted by Davud Zulke at
    http://pastie.org/private/cl3raamde9jh1ldxar59a
    
    See: http://www.propelorm.org/ticket/1360

--- runtime/lib/adapter/DBAdapter.php
+++ runtime/lib/adapter/DBAdapter.php
@@ -73,6 +73,14 @@ abstract class DBAdapter
 	}
 
 	/**
+	 * @see        DBAdapter::initConnection()
+	 */
+	public function prepareParams(array $settings)
+	{
+		return $settings;
+	}
+
+	/**
 	 * This method is called after a connection was created to run necessary
 	 * post-initialization queries or code.
 	 *
@@ -266,17 +274,17 @@ abstract class DBAdapter
 		/** @var $dt PropelDateTime */
 		if ($dt = PropelDateTime::newInstance($value)) {
 			switch($cMap->getType()) {
-				case PropelColumnTypes::TIMESTAMP:
-				case PropelColumnTypes::BU_TIMESTAMP:
-					$value = $dt->format($this->getTimestampFormatter());
-					break;
-				case PropelColumnTypes::DATE:
-				case PropelColumnTypes::BU_DATE:
-					$value = $dt->format($this->getDateFormatter());
-					break;
-				case PropelColumnTypes::TIME:
-					$value = $dt->format($this->getTimeFormatter());
-					break;
+			case PropelColumnTypes::TIMESTAMP:
+			case PropelColumnTypes::BU_TIMESTAMP:
+				$value = $dt->format($this->getTimestampFormatter());
+				break;
+			case PropelColumnTypes::DATE:
+			case PropelColumnTypes::BU_DATE:
+				$value = $dt->format($this->getDateFormatter());
+				break;
+			case PropelColumnTypes::TIME:
+				$value = $dt->format($this->getTimeFormatter());
+				break;
 			}
 		}
 		return $value;
@@ -450,9 +458,9 @@ abstract class DBAdapter
 
 		// Build the SQL from the arrays we compiled
 		$sql =  "SELECT "
-		. ($queryComment ? '/* ' . $queryComment . ' */ ' : '')
-		. ($selectModifiers ? (implode(' ', $selectModifiers) . ' ') : '')
-		. implode(", ", $selectClause);
+			. ($queryComment ? '/* ' . $queryComment . ' */ ' : '')
+			. ($selectModifiers ? (implode(' ', $selectModifiers) . ' ') : '')
+			. implode(", ", $selectClause);
 
 		return $sql;
 	}
--- runtime/lib/adapter/DBMySQL.php
+++ runtime/lib/adapter/DBMySQL.php
@@ -197,4 +197,26 @@ class DBMySQL extends DBAdapter
 
 		return $stmt->bindValue($parameter, $value, $pdoType);
 	}
+
+	public function prepareParams(array $params)
+	{
+		$params = parent::prepareParams($params);
+
+		if(isset($params['settings']['charset']['value'])) {
+			if(version_compare(PHP_VERSION, '5.3.6', '<')) {
+				throw new PropelException(<<<EXCEPTION
+Connection option "charset" cannot be used for MySQL connections in PHP versions older than 5.3.6.
+Please refer to http://www.propelorm.org/ticket/1360 for instructions and details about the implications of
+using a SET NAMES statement in the "queries" setting.
+EXCEPTION
+				);
+			} else {
+				if(strpos($params['dsn'], ';charset=') === false) {
+					$params['dsn'] .= ';charset=' . $params['settings']['charset']['value'];
+				}
+			}
+		}
+
+		return $params;
+	}
 }
