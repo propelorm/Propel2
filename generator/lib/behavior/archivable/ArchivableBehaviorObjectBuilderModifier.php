f1a163d1d31f66104221349951b1314bdd663a37
Francois Zaninotto <fzaninotto@gmail.com>
2011-08-22 22:18:08 +0200

    [archivable behavior] split ArchivableBehavior class to make it more readable and ready for future additions

---
+++ generator/lib/behavior/archivable/ArchivableBehaviorObjectBuilderModifier.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+/**
+ * Keeps tracks of an ActiveRecord object, even after deletion
+ *
+ * @author     François Zaninotto
+ * @package    propel.generator.behavior.archivable
+ */
+class ArchivableBehaviorObjectBuilderModifier
+{
+	protected $behavior, $table, $builder;
+
+	public function __construct($behavior)
+	{
+		$this->behavior = $behavior;
+		$this->table = $behavior->getTable();
+	}
+
+	protected function getParameter($key)
+	{
+		return $this->behavior->getParameter($key);
+	}
+
+	/**
+	 * @return string the PHP code to be added to the builder
+	 */
+	public function objectMethods($builder)
+	{
+		$this->builder = $builder;
+		$script = '';
+		$script .= $this->addObjectArchive($builder);
+		$script .= $this->addObjectPopulateFromArchive($builder);
+		return $script;
+	}
+
+	/**
+	 * @return string the PHP code to be added to the builder
+	 */
+	public function addObjectArchive($builder)
+	{
+		$archiveTablePhpName = $this->behavior->getArchiveTablePhpName($builder);
+		$archiveTableQueryName = $this->behavior->getArchiveTableQueryName($builder);
+		$script = "
+/**
+ * Copy the data of the current object into a $archiveTablePhpName archive object.
+ * The archived object is then saved.
+ * If the current object has already been archived, the archived object
+ * is updated and not duplicated.
+ *
+ * @param PropelPDO \$con Optional connection object
+ *
+ * @return     " . $this->builder->getObjectClassname() . " The current object (for fluent API support)
+ */
+public function archive(PropelPDO \$con = null)
+{
+	\$archive = " . $archiveTableQueryName . "::create()
+		->filterByPrimaryKey(\$this->getPrimaryKey())
+		->findOneOrCreate(\$con);
+	\$this->copyInto(\$archive, \$deepCopy = false, \$makeNew = false);";
+		if ($archivedAtColumn = $this->behavior->getArchivedAtColumn()) {
+			$script .= "
+	\$archive->set" . $archivedAtColumn->getPhpName()."(time());";
+		}
+		$script .= "
+	\$archive->save(\$con);
+
+	return \$this;
+}
+";
+		return $script;
+	}
+
+	/**
+	 * Generates a method to populate the current AR object based on an archive object.
+	 * This method is necessary because the archive's copyInto() may include the archived_at column
+	 * and therefore cannot be used. Besides, the way autoincremented PKs are handled should be explicit.
+	 *
+	 * @return string the PHP code to be added to the builder
+	 */
+	public function addObjectPopulateFromArchive($builder)
+	{
+		$archiveTablePhpName = $this->behavior->getArchiveTablePhpName($builder);
+		$usesAutoIncrement = $this->table->hasAutoIncrementPrimaryKey();
+		$script = "
+/**
+ * Populates the the current object based on a $archiveTablePhpName archive object.
+ *
+ * @param      " . $archiveTablePhpName . " \$archive An archived object based on the same class";
+ 		if ($usesAutoIncrement) {
+ 			$script .= "
+ * @param      Boolean \$populateAutoIncrementPrimaryKeys 
+ *               If true, autoincrement columns are copied from the archive object.
+ *               If false, autoincrement columns are left intact.";
+ 		}
+ 		$script .= "
+ *
+ * @return     " . $this->builder->getObjectClassname() . " The current object (for fluent API support)
+ */
+public function populateFromArchive(\$archive" . ($usesAutoIncrement ? ", \$populateAutoIncrementPrimaryKeys = false" : '') . ")
+{";
+		if ($usesAutoIncrement) {
+			$script .= "
+	if (\$populateAutoIncrementPrimaryKeys) {";
+			foreach ($this->table->getColumns() as $col) {
+				$snippet = "";
+				if ($col->isAutoIncrement()) {
+					$script .= "
+		\$this->set" . $col->getPhpName() . "(\$archive->get" . $col->getPhpName() . "());";
+				}
+			}
+			$script .= "
+	}";
+		}
+		foreach ($this->table->getColumns() as $col) {
+			if ($col->isAutoIncrement()) {
+				continue;
+			}
+			$script .= "
+	\$this->set" . $col->getPhpName() . "(\$archive->get" . $col->getPhpName() . "());";
+		}
+		$script .= "
+
+	return \$this;
+}
+";
+		return $script;
+	}
+
+	/**
+	 * @return string the PHP code to be added to the builder
+	 */
+	public function postInsert($builder)
+	{
+		if ($this->getParameter('archive_on_insert') == 'true') {
+			return "\$this->archive(\$con);";
+		}
+	}
+
+	/**
+	 * @return string the PHP code to be added to the builder
+	 */
+	public function postUpdate($builder)
+	{
+		if ($this->getParameter('archive_on_update') == 'true') {
+			return "\$this->archive(\$con);";
+		}
+	}
+
+	/**
+	 * @return string the PHP code to be added to the builder
+	 */
+	public function postDelete($builder)
+	{
+		if ($this->getParameter('archive_on_delete') == 'true') {
+			return "\$this->archive(\$con);";
+		}
+	}
+
+
+}
---
+++ test/testsuite/generator/behavior/archivable/ArchivableBehaviorObjectBuilderModifierTest.php
@@ -0,0 +1,321 @@
+<?php
+
+/*
+ *	$Id$
+ * This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+require_once dirname(__FILE__) . '/../../../../../generator/lib/util/PropelQuickBuilder.php';
+require_once dirname(__FILE__) . '/../../../../../generator/lib/behavior/archivable/ArchivableBehavior.php';
+require_once dirname(__FILE__) . '/../../../../../runtime/lib/Propel.php';
+
+/**
+ * Tests for ArchivableBehavior class
+ *
+ * @author     François Zaninotto
+ * @version    $Revision$
+ * @package    generator.behavior.archivable
+ */
+class ArchivableBehaviorObjectBuilderModifierTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		if (!class_exists('ArchivableTest10')) {
+			$schema = <<<EOF
+<database name="archivable_behavior_test_10">
+
+	<table name="archivable_test_10">
+		<column name="id" required="true" primaryKey="true" autoIncrement="true" type="INTEGER" />
+		<column name="title" type="VARCHAR" size="100" primaryString="true" />
+		<column name="age" type="INTEGER" />
+		<column name="foo_id" type="INTEGER" />
+		<foreign-key foreignTable="archivable_test_20">
+			<reference local="foo_id" foreign="id" />
+		</foreign-key>
+		<index>
+			<index-column name="title" />
+			<index-column name="age" />
+		</index>
+		<behavior name="archivable" />
+	</table>
+
+	<table name="archivable_test_20">
+		<column name="id" required="true" primaryKey="true" autoIncrement="true" type="INTEGER" />
+		<column name="title" type="VARCHAR" size="100" primaryString="true" />
+		<behavior name="archivable" />
+	</table>
+
+	<table name="archivable_test_20_archive">
+		<column name="id" required="true" primaryKey="true" type="INTEGER" />
+		<column name="title" type="VARCHAR" size="100" primaryString="true" />
+	</table>
+
+	<table name="archivable_test_30">
+		<column name="id" required="true" primaryKey="true" autoIncrement="true" type="INTEGER" />
+		<column name="title" type="VARCHAR" size="100" primaryString="true" />
+		<column name="age" type="INTEGER" />
+		<column name="foo_id" type="INTEGER" />
+		<behavior name="archivable">
+			<parameter name="log_archived_at" value="false" />
+			<parameter name="archive_table" value="my_old_archivable_test_30" />
+			<parameter name="archive_on_insert" value="true" />
+			<parameter name="archive_on_update" value="true" />
+			<parameter name="archive_on_delete" value="false" />
+		</behavior>
+	</table>
+
+	<table name="archivable_test_40">
+		<column name="id" required="true" primaryKey="true" autoIncrement="true" type="INTEGER" />
+		<column name="title" type="VARCHAR" size="100" primaryString="true" />
+		<column name="age" type="INTEGER" />
+		<behavior name="archivable">
+			<parameter name="archive_class" value="FooArchive" />
+		</behavior>
+	</table>
+
+</database>
+EOF;
+			PropelQuickBuilder::buildSchema($schema);
+		}
+	}
+
+	public function testHasArchiveMethod()
+	{
+		$this->assertTrue(method_exists('ArchivableTest10', 'archive'));
+	}
+
+	public function testArchiveCreatesACopyByDefault()
+	{
+		ArchivableTest10ArchiveQuery::create()->deleteAll();
+		$a = new ArchivableTest10();
+		$a->setTitle('foo');
+		$a->setAge(12);
+		$a->save();
+		$a->archive();
+		$archive = ArchivableTest10ArchiveQuery::create()
+			->filterById($a->getId())
+			->findOne();
+		$this->assertInstanceOf('ArchivableTest10Archive', $archive);
+		$this->assertEquals('foo', $archive->getTitle());
+		$this->assertEquals(12, $archive->getAge());
+	}
+
+	public function testArchiveUpdatesExistingArchive()
+	{
+		ArchivableTest10ArchiveQuery::create()->deleteAll();
+		$a = new ArchivableTest10();
+		$a->setTitle('foo');
+		$a->setAge(12);
+		$a->save();
+		$b = new ArchivableTest10Archive();
+		$b->setId($a->getId());
+		$b->setTitle('bar');
+		$b->save();
+		$a->archive();
+		$this->assertEquals(1, ArchivableTest10ArchiveQuery::create()->count());
+		$this->assertEquals('foo', $b->getTitle());
+	}
+
+	public function testArchiveUsesArchiveClassIfSpecified()
+	{
+		$a = new ArchivableTest40();
+		$a->setTitle('foo');
+		$a->setAge(12);
+		$a->save();
+		$a->archive();
+		$archive = FooArchiveCollection::getArchiveSingleton();
+		$this->assertEquals($a->getId(), $archive->id);
+		$this->assertEquals('foo', $archive->title);
+		$this->assertEquals(12, $archive->age);
+	}
+
+	public function testArchiveReturnsCurrentObject()
+	{
+		$a = new ArchivableTest10();
+		$ret = $a->archive();
+		$this->assertSame($ret, $a);
+	}
+
+	public function testInsertDoesNotCreateArchiveByDefault()
+	{
+		$a = new ArchivableTest10();
+		$a->setTitle('foo');
+		$a->setAge(12);
+		ArchivableTest10ArchiveQuery::create()->deleteAll();
+		$a->save();
+		$this->assertEquals(0, ArchivableTest10ArchiveQuery::create()->count());
+	}
+
+	public function testInsertCreatesArchiveIfSpecified()
+	{
+		$a = new ArchivableTest30();
+		$a->setTitle('foo');
+		$a->setAge(12);
+		MyOldArchivableTest30Query::create()->deleteAll();
+		$a->save();
+		$this->assertEquals(1, MyOldArchivableTest30Query::create()->count());
+		$archive = MyOldArchivableTest30Query::create()
+			->filterById($a->getId())
+			->findOne();
+		$this->assertInstanceOf('MyOldArchivableTest30', $archive);
+		$this->assertEquals('foo', $archive->getTitle());
+		$this->assertEquals(12, $archive->getAge());
+	}
+
+	public function testUpdateDoesNotCreateArchiveByDefault()
+	{
+		$a = new ArchivableTest10();
+		$a->setTitle('foo');
+		$a->setAge(12);
+		$a->save();
+		$a->setTitle('bar');
+		ArchivableTest10ArchiveQuery::create()->deleteAll();
+		$a->save();
+		$this->assertEquals(0, ArchivableTest10ArchiveQuery::create()->count());
+	}
+
+	public function testUpdateCreatesArchiveIfSpecified()
+	{
+		$a = new ArchivableTest30();
+		$a->setTitle('foo');
+		$a->setAge(12);
+		$a->save();
+		$a->setTitle('bar');
+		MyOldArchivableTest30Query::create()->deleteAll();
+		$a->save();
+		$this->assertEquals(1, MyOldArchivableTest30Query::create()->count());
+		$archive = MyOldArchivableTest30Query::create()
+			->filterById($a->getId())
+			->findOne();
+		$this->assertInstanceOf('MyOldArchivableTest30', $archive);
+		$this->assertEquals('bar', $archive->getTitle());
+		$this->assertEquals(12, $archive->getAge());
+	}
+
+	public function testDeleteCreatesArchiveByDefault()
+	{
+		$a = new ArchivableTest10();
+		$a->setTitle('foo');
+		$a->setAge(12);
+		$a->save();
+		ArchivableTest10ArchiveQuery::create()->deleteAll();
+		$a->delete();
+		$this->assertEquals(1, ArchivableTest10ArchiveQuery::create()->count());
+		$archive = ArchivableTest10ArchiveQuery::create()
+			->filterById($a->getId())
+			->findOne();
+		$this->assertInstanceOf('ArchivableTest10Archive', $archive);
+		$this->assertEquals('foo', $archive->getTitle());
+		$this->assertEquals(12, $archive->getAge());
+	}
+
+	public function testDeleteDoesNotCreateArchiveIfSpecified()
+	{
+		$a = new ArchivableTest30();
+		$a->setTitle('foo');
+		$a->setAge(12);
+		$a->save();
+		MyOldArchivableTest30Query::create()->deleteAll();
+		$a->delete();
+		$this->assertEquals(0, MyOldArchivableTest30Query::create()->count());
+	}
+
+	public function testHasPopulateFromArchiveMethod()
+	{
+		$this->assertTrue(method_exists('ArchivableTest10', 'populateFromArchive'));
+	}
+
+	public function testPopulateFromArchiveReturnsCurrentObject()
+	{
+		$archive = new ArchivableTest10Archive();
+		$a = new ArchivableTest10();
+		$ret = $a->populateFromArchive($archive);
+		$this->assertSame($ret, $a);
+	}
+
+	public function testPopulateFromArchive()
+	{
+		ArchivableTest10ArchiveQuery::create()->deleteAll();
+		ArchivableTest10Query::create()->deleteAll();
+		$archive = new ArchivableTest10Archive();
+		$archive->setId(123); // not autoincremented
+		$archive->setTitle('foo');
+		$archive->setAge(12);
+		$archive->save();
+		$a = new ArchivableTest10();
+		$a->populateFromArchive($archive);
+		$this->assertNotEquals(123, $a->getId());
+		$this->assertEquals('foo', $a->getTitle());
+		$this->assertEquals(12, $a->getAge());
+		$b = new ArchivableTest10();
+		$b->populateFromArchive($archive, true);
+		$this->assertEquals(123, $b->getId());
+	}
+
+}
+
+
+class FooArchiveQuery
+{
+	protected $pk;
+
+	public static function create()
+	{
+		return new self();
+	}
+
+	public function filterByPrimaryKey($pk)
+	{
+		$this->pk = $pk;
+		return $this;
+	}
+
+	public function findOneOrCreate()
+	{
+		$archive = FooArchiveCollection::getArchiveSingleton();
+		$archive->setId($this->pk);
+		return $archive;
+	}
+}
+
+class FooArchive
+{
+	public $id, $title, $age;
+
+	public function setId($value)
+	{
+		$this->id = $value;
+	}
+
+	public function setTitle($value)
+	{
+		$this->title = $value;
+	}
+
+	public function setAge($value)
+	{
+		$this->age = $value;
+	}
+
+	public function save()
+	{
+		return $this;
+	}
+}
+
+class FooArchiveCollection
+{
+	protected static $instance;
+
+	public static function getArchiveSingleton()
+	{
+		if (null === self::$instance) {
+			self::$instance = new FooArchive();
+		}
+		return self::$instance;
+	}
+}
