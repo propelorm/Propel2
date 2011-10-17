4bfc86a39273a89b34384e81316ccba9820db96a
David Herrmann <david@vworld.at>
2011-10-17 11:19:14 +0200

    Added *Compat task classes and namespaces for migration related tasks

---
+++ src/Propel/Generator/Task/PropelMigrationStatusTaskCompat.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!class_exists('\Propel\Generator\Task\BasePropelMigrationTask')) {
+	require_once __DIR__ . '/../../../../autoload.php.dist';
+}
+
+class PropelMigrationStatusTaskCompat extends \Propel\Generator\Task\PropelMigrationStatusTask
+{
+}
