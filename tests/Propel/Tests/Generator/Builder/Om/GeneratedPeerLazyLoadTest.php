2a66cd1609b0c4d0b42f247aff5d14537e70752b
William DURAND <william.durand1@gmail.com>
2011-10-19 20:13:47 +0200

    [Generator] [Tests] Fixed tests for GeneratedPeerLazyLoadTest

--- tests/Propel/Tests/Generator/Builder/Om/GeneratedPeerLazyLoadTest.php
+++ tests/Propel/Tests/Generator/Builder/Om/GeneratedPeerLazyLoadTest.php
@@ -23,7 +23,7 @@ class GeneratedPeerLazyLoadTest extends \PHPUnit_Framework_TestCase
 {
 	public function setUp()
 	{
-		if (!class_exists('LazyLoadActiveRecord2')) {
+		if (!class_exists('\LazyLoadActiveRecord2')) {
 			$schema = <<<EOF
 <database name="lazy_load_active_record_2">
 	<table name="lazy_load_active_record_2">
@@ -40,15 +40,15 @@ EOF;
 
 	public function testNumHydrateColumns()
 	{
-		$this->assertEquals(3, LazyLoadActiveRecord2Peer::NUM_HYDRATE_COLUMNS);
+		$this->assertEquals(3, \LazyLoadActiveRecord2Peer::NUM_HYDRATE_COLUMNS);
 	}
 
 	public function testPopulateObjectNotInPool()
 	{
-		LazyLoadActiveRecord2Peer::clearInstancePool();
+		\LazyLoadActiveRecord2Peer::clearInstancePool();
 		$values = array(123, 'fooValue', 'bazValue');
 		$col = 0;
-		list($obj, $col) = LazyLoadActiveRecord2Peer::populateObject($values, $col);
+		list($obj, $col) = \LazyLoadActiveRecord2Peer::populateObject($values, $col);
 		$this->assertEquals(3, $col);
 		$this->assertEquals(123, $obj->getId());
 		$this->assertEquals('fooValue', $obj->getFoo());
@@ -58,16 +58,16 @@ EOF;
 
 	public function testPopulateObjectInPool()
 	{
-		LazyLoadActiveRecord2Peer::clearInstancePool();
-		$ar = new LazyLoadActiveRecord2();
+		\LazyLoadActiveRecord2Peer::clearInstancePool();
+		$ar = new \LazyLoadActiveRecord2();
 		$ar->setId(123);
 		$ar->setFoo('fooValue');
 		$ar->setBaz('bazValue');
 		$ar->setNew(false);
-		LazyLoadActiveRecord2Peer::addInstanceToPool($ar, 123);
+		\LazyLoadActiveRecord2Peer::addInstanceToPool($ar, 123);
 		$values = array(123, 'fooValue', 'bazValue');
 		$col = 0;
-		list($obj, $col) = LazyLoadActiveRecord2Peer::populateObject($values, $col);
+		list($obj, $col) = \LazyLoadActiveRecord2Peer::populateObject($values, $col);
 		$this->assertEquals(3, $col);
 		$this->assertEquals(123, $obj->getId());
 		$this->assertEquals('fooValue', $obj->getFoo());
@@ -77,10 +77,10 @@ EOF;
 
 	public function testPopulateObjectNotInPoolStartColGreaterThanOne()
 	{
-		LazyLoadActiveRecord2Peer::clearInstancePool();
+		\LazyLoadActiveRecord2Peer::clearInstancePool();
 		$values = array('dummy', 'dummy', 123, 'fooValue', 'bazValue', 'dummy');
 		$col = 2;
-		list($obj, $col) = LazyLoadActiveRecord2Peer::populateObject($values, $col);
+		list($obj, $col) = \LazyLoadActiveRecord2Peer::populateObject($values, $col);
 		$this->assertEquals(5, $col);
 		$this->assertEquals(123, $obj->getId());
 		$this->assertEquals('fooValue', $obj->getFoo());
@@ -90,16 +90,16 @@ EOF;
 
 	public function testPopulateObjectInPoolStartColGreaterThanOne()
 	{
-		LazyLoadActiveRecord2Peer::clearInstancePool();
-		$ar = new LazyLoadActiveRecord2();
+		\LazyLoadActiveRecord2Peer::clearInstancePool();
+		$ar = new \LazyLoadActiveRecord2();
 		$ar->setId(123);
 		$ar->setFoo('fooValue');
 		$ar->setBaz('bazValue');
 		$ar->setNew(false);
-		LazyLoadActiveRecord2Peer::addInstanceToPool($ar, 123);
+		\LazyLoadActiveRecord2Peer::addInstanceToPool($ar, 123);
 		$values = array('dummy', 'dummy', 123, 'fooValue', 'bazValue', 'dummy');
 		$col = 2;
-		list($obj, $col) = LazyLoadActiveRecord2Peer::populateObject($values, $col);
+		list($obj, $col) = \LazyLoadActiveRecord2Peer::populateObject($values, $col);
 		$this->assertEquals(5, $col);
 		$this->assertEquals(123, $obj->getId());
 		$this->assertEquals('fooValue', $obj->getFoo());
