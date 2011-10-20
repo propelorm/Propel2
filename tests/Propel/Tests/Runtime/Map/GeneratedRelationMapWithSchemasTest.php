b6e50e914186b65bca95ee7d158de5fbae9c1735
William DURAND <william.durand1@gmail.com>
2011-10-20 18:27:41 +0200

    [Runtime] [Tests] Fixed all tests in Map/

--- tests/Propel/Tests/Runtime/Map/GeneratedRelationMapWithSchemasTest.php
+++ tests/Propel/Tests/Runtime/Map/GeneratedRelationMapWithSchemasTest.php
@@ -10,9 +10,10 @@
 
 namespace Propel\Tests\Runtime\Map;
 
-use Propel\Runtime\Propel;
-
 use Propel\Tests\Helpers\Schemas\SchemasTestBase;
+
+use Propel\Runtime\Propel;
+use Propel\Runtime\Map\RelationMap;
 
 /**
  * Test class for PHP5TableMapBuilder with schemas.
@@ -33,45 +34,45 @@ class GeneratedRelationMapWithSchemasTest extends SchemasTestBase
 
 	public function testGetRightTable()
 	{
-		$bookTable = $this->databaseMap->getTableByPhpName('BookstoreSchemasBookstore');
-		$contestTable = $this->databaseMap->getTableByPhpName('ContestBookstoreContest');
+		$bookTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\Bookstore');
+		$contestTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\BookstoreContest');
 		$this->assertEquals(
 			$bookTable->getName(),
-			$contestTable->getRelation('BookstoreSchemasBookstore')->getRightTable()->getName(),
+			$contestTable->getRelation('Bookstore')->getRightTable()->getName(),
 			'getRightTable() returns correct table when called on a many to one relationship'
 		);
 		$this->assertEquals(
 			$contestTable->getName(),
-			$bookTable->getRelation('ContestBookstoreContest')->getRightTable()->getName(),
+			$bookTable->getRelation('BookstoreContest')->getRightTable()->getName(),
 			'getRightTable() returns correct table when called on a one to many relationship'
 		);
-		$bookCustomerTable = $this->databaseMap->getTableByPhpName('BookstoreSchemasCustomer');
-		$bookCustomerAccTable = $this->databaseMap->getTableByPhpName('BookstoreSchemasCustomerAccount');
+		$bookCustomerTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\Customer');
+		$bookCustomerAccTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\CustomerAccount');
 		$this->assertEquals(
 			$bookCustomerAccTable->getName(),
-			$bookCustomerTable->getRelation('BookstoreSchemasCustomerAccount')->getRightTable()->getName(),
+			$bookCustomerTable->getRelation('CustomerAccount')->getRightTable()->getName(),
 			'getRightTable() returns correct table when called on a one to one relationship'
 		);
 		$this->assertEquals(
 			$bookCustomerTable->getName(),
-			$bookCustomerAccTable->getRelation('BookstoreSchemasCustomer')->getRightTable()->getName(),
+			$bookCustomerAccTable->getRelation('Customer')->getRightTable()->getName(),
 			'getRightTable() returns correct table when called on a one to one relationship'
 		);
 	}
 
 	public function testColumnMappings()
 	{
-		$contestTable = $this->databaseMap->getTableByPhpName('ContestBookstoreContest');
-		$this->assertEquals(array('contest.bookstore_contest.BOOKSTORE_ID' => 'bookstore_schemas.bookstore.ID'), $contestTable->getRelation('BookstoreSchemasBookstore')->getColumnMappings(), 'getColumnMappings returns local to foreign by default');
-		$this->assertEquals(array('contest.bookstore_contest.BOOKSTORE_ID' => 'bookstore_schemas.bookstore.ID'), $contestTable->getRelation('BookstoreSchemasBookstore')->getColumnMappings(RelationMap::LEFT_TO_RIGHT), 'getColumnMappings returns local to foreign when asked left to right for a many to one relationship');
+		$contestTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\BookstoreContest');
+		$this->assertEquals(array('contest.bookstore_contest.BOOKSTORE_ID' => 'bookstore_schemas.bookstore.ID'), $contestTable->getRelation('Bookstore')->getColumnMappings(), 'getColumnMappings returns local to foreign by default');
+		$this->assertEquals(array('contest.bookstore_contest.BOOKSTORE_ID' => 'bookstore_schemas.bookstore.ID'), $contestTable->getRelation('Bookstore')->getColumnMappings(RelationMap::LEFT_TO_RIGHT), 'getColumnMappings returns local to foreign when asked left to right for a many to one relationship');
 
-		$bookTable = $this->databaseMap->getTableByPhpName('BookstoreSchemasBookstore');
-		$this->assertEquals(array('contest.bookstore_contest.BOOKSTORE_ID' => 'bookstore_schemas.bookstore.ID'), $bookTable->getRelation('ContestBookstoreContest')->getColumnMappings(), 'getColumnMappings returns local to foreign by default');
-		$this->assertEquals(array('bookstore_schemas.bookstore.ID' => 'contest.bookstore_contest.BOOKSTORE_ID'), $bookTable->getRelation('ContestBookstoreContest')->getColumnMappings(RelationMap::LEFT_TO_RIGHT), 'getColumnMappings returns foreign to local when asked left to right for a one to many relationship');
+        $bookTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\Bookstore');
+		$this->assertEquals(array('contest.bookstore_contest.BOOKSTORE_ID' => 'bookstore_schemas.bookstore.ID'), $bookTable->getRelation('BookstoreContest')->getColumnMappings(), 'getColumnMappings returns local to foreign by default');
+		$this->assertEquals(array('bookstore_schemas.bookstore.ID' => 'contest.bookstore_contest.BOOKSTORE_ID'), $bookTable->getRelation('BookstoreContest')->getColumnMappings(RelationMap::LEFT_TO_RIGHT), 'getColumnMappings returns foreign to local when asked left to right for a one to many relationship');
 
-		$bookCustomerTable = $this->databaseMap->getTableByPhpName('BookstoreSchemasCustomer');
-		$this->assertEquals(array('bookstore_schemas.customer_account.CUSTOMER_ID' => 'bookstore_schemas.customer.ID'), $bookCustomerTable->getRelation('BookstoreSchemasCustomerAccount')->getColumnMappings(), 'getColumnMappings returns local to foreign by default');
-		$this->assertEquals(array('bookstore_schemas.customer.ID' => 'bookstore_schemas.customer_account.CUSTOMER_ID'), $bookCustomerTable->getRelation('BookstoreSchemasCustomerAccount')->getColumnMappings(RelationMap::LEFT_TO_RIGHT), 'getColumnMappings returns foreign to local when asked left to right for a one to one relationship');
+		$bookCustomerTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\Customer');
+		$this->assertEquals(array('bookstore_schemas.customer_account.CUSTOMER_ID' => 'bookstore_schemas.customer.ID'), $bookCustomerTable->getRelation('CustomerAccount')->getColumnMappings(), 'getColumnMappings returns local to foreign by default');
+		$this->assertEquals(array('bookstore_schemas.customer.ID' => 'bookstore_schemas.customer_account.CUSTOMER_ID'), $bookCustomerTable->getRelation('CustomerAccount')->getColumnMappings(RelationMap::LEFT_TO_RIGHT), 'getColumnMappings returns foreign to local when asked left to right for a one to one relationship');
 	}
 
 }
--- tests/Propel/Tests/Runtime/Map/RelatedMapSymmetricalWithSchemasTest.php
+++ tests/Propel/Tests/Runtime/Map/RelatedMapSymmetricalWithSchemasTest.php
@@ -34,41 +34,40 @@ class RelatedMapSymmetricalWithSchemasTest extends SchemasTestBase
 	public function testOneToMany()
 	{
 		// passes on its own, but not with the full tests suite
-		$this->markTestSkipped();
-		$contestTable = $this->databaseMap->getTableByPhpName('\Propel\Tests\BookstoreSchemas\bookstore_schemas\Map\ContestBookstoreContest');
-		$contestToBookstore = $contestTable->getRelation('BookstoreSchemasBookstore');
-		$bookstoreTable = $this->databaseMap->getTableByPhpName('\Propel\Tests\BookstoreSchemas\bookstore_schemas\Map\BookstoreSchemasBookstore');
-		$bookstoreToContest = $bookstoreTable->getRelation('ContestBookstoreContest');
+		$contestTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\BookstoreContest');
+		$contestToBookstore = $contestTable->getRelation('Bookstore');
+		$bookstoreTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\Bookstore');
+		$bookstoreToContest = $bookstoreTable->getRelation('BookstoreContest');
 		$this->assertEquals($bookstoreToContest->getName(), $contestToBookstore->getSymmetricalRelation()->getName());
 		$this->assertEquals($contestToBookstore->getName(), $bookstoreToContest->getSymmetricalRelation()->getName());
 	}
 
 	public function testOneToOne()
 	{
-		$accountTable = $this->databaseMap->getTableByPhpName('\Propel\Tests\BookstoreSchemas\bookstore_schemas\Mapa\BookstoreSchemasCustomerAccount');
-		$accountToCustomer = $accountTable->getRelation('BookstoreSchemasCustomer');
-		$customerTable = $this->databaseMap->getTableByPhpName('\Propel\Tests\BookstoreSchemas\bookstore_schemas\Map\BookstoreSchemasCustomer');
-		$customerToAccount = $customerTable->getRelation('BookstoreSchemasCustomerAccount');
+		$accountTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\CustomerAccount');
+		$accountToCustomer = $accountTable->getRelation('Customer');
+		$customerTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\Customer');
+		$customerToAccount = $customerTable->getRelation('CustomerAccount');
 		$this->assertEquals($accountToCustomer, $customerToAccount->getSymmetricalRelation());
 		$this->assertEquals($customerToAccount, $accountToCustomer->getSymmetricalRelation());
 	}
 
 	public function testSeveralRelationsOnSameTable()
 	{
-		$contestTable = $this->databaseMap->getTableByPhpName('\Propel\Tests\BookstoreSchemas\bookstore_schemas\Map\ContestBookstoreContest');
-		$contestToCustomer = $contestTable->getRelation('BookstoreSchemasCustomerRelatedByFirstContest');
-		$customerTable = $this->databaseMap->getTableByPhpName('\Propel\Tests\BookstoreSchemas\bookstore_schemas\Map\BookstoreSchemasCustomer');
-		$customerToContest = $customerTable->getRelation('ContestBookstoreContestRelatedByFirstContest');
+		$contestTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\BookstoreContest');
+		$contestToCustomer = $contestTable->getRelation('CustomerRelatedByFirstContest');
+		$customerTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\Customer');
+		$customerToContest = $customerTable->getRelation('BookstoreContestRelatedByFirstContest');
 		$this->assertEquals($contestToCustomer, $customerToContest->getSymmetricalRelation());
 		$this->assertEquals($customerToContest, $contestToCustomer->getSymmetricalRelation());
 	}
 
 	public function testCompositeForeignKey()
 	{
-		$entryTable = $this->databaseMap->getTableByPhpName('\Propel\Tests\BookstoreSchemas\bookstore_schemas\Map\ContestBookstoreContestEntry');
-		$entryToContest = $entryTable->getRelation('ContestBookstoreContest');
-		$contestTable = $this->databaseMap->getTableByPhpName('\Propel\Tests\BookstoreSchemas\bookstore_schemas\Map\ContestBookstoreContest');
-		$contestToEntry = $contestTable->getRelation('ContestBookstoreContestEntry');
+		$entryTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\BookstoreContestEntry');
+		$entryToContest = $entryTable->getRelation('BookstoreContest');
+		$contestTable = $this->databaseMap->getTableByPhpName('Propel\Tests\BookstoreSchemas\BookstoreContest');
+		$contestToEntry = $contestTable->getRelation('BookstoreContestEntry');
 		$this->assertEquals($entryToContest, $contestToEntry->getSymmetricalRelation());
 		$this->assertEquals($contestToEntry, $entryToContest->getSymmetricalRelation());
 	}
