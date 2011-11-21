9513f4920e478ec55fc86bf6da5c584827b8f0ba
William DURAND <william.durand1@gmail.com>
2011-11-21 18:31:14 +0100

    Introduced Propel commands system

--- bin/install_vendors.sh
+++ bin/install_vendors.sh
@@ -2,3 +2,4 @@
 
 git clone https://github.com/symfony/ClassLoader.git vendor/Symfony/Component/ClassLoader
 git clone https://github.com/symfony/Yaml.git vendor/Symfony/Component/Yaml
+git clone https://github.com/symfony/Console.git vendor/Symfony/Component/Console
---
+++ bin/propel.php
@@ -0,0 +1,16 @@
+#!/usr/local/bin/php
+<?php
+
+if (!class_exists('\Symfony\Component\Console\Application')) {
+    if (file_exists($file = __DIR__.'/../autoload.php')) {
+        require_once $file;
+    } elseif (file_exists($file = __DIR__.'/../autoload.php.dist')) {
+        require_once $file;
+    }
+}
+
+use \Symfony\Component\Console\Application;
+
+$app = new Application('Propel', '2.0 (dev)');
+//$app->add(new SimpleCommand());
+$app->run();
