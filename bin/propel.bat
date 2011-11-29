2a27158e5022230b89374d6757ecbf85e1775a9c
William DURAND <william.durand1@gmail.com>
2011-11-29 16:30:54 +0100

    [Console] Added propel batch

---
+++ bin/propel.bat
@@ -0,0 +1,9 @@
+@echo off
+
+if "%PHPBIN%" == "" set PHPBIN=php
+if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
+GOTO RUN
+:USE_PEAR_PATH
+set PHPBIN=%PHP_PEAR_PHP_BIN%
+:RUN
+"%PHPBIN%" "bin\propel" %*
