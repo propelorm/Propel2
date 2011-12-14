fc1e20cc5f1de9c2a1a290db1cd23303b523d168
William DURAND <william.durand1@gmail.com>
2011-12-14 21:14:42 +0100

    [Runtime][Parser] Added named exceptions

---
+++ src/Propel/Runtime/Exception/FileNotFoundException.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Propel\Runtime\Exception;
+
+/**
+ * @author William Durand <william.durand1@gmail.com>
+ */
+class FileNotFoundException extends RuntimeException
+{
+    /**
+     * @param string $path  The path to the file that was not found
+     */
+    public function __construct($path)
+    {
+        parent::__construct(sprintf('The file "%s" does not exist', $path));
+    }
+}
