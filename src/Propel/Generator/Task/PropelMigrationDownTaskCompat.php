8d1ff79834e8d4aac370037e494d1184ba8b94c4
William DURAND <william.durand1@gmail.com>
2011-12-22 22:50:03 +0100

    [Generator] Removed old tasks (migration up/down)

--- src/Propel/Generator/Task/PropelMigrationDownTaskCompat.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!class_exists('\Propel\Generator\Task\BasePropelMigrationTask')) {
-    require_once __DIR__ . '/../../../../autoload.php.dist';
-}
-
-class PropelMigrationDownTaskCompat extends \Propel\Generator\Task\PropelMigrationDownTask
-{
-}
