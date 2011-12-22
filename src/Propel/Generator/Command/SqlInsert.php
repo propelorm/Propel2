9389fe4757602e769cfcf553288f074f9580966b
William DURAND <william.durand1@gmail.com>
2011-12-22 21:26:35 +0100

    [Command] Introduced sql:insert command

---
+++ src/Propel/Generator/Command/SqlInsert.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Propel\Generator\Command;
+
+
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+
+
+use Propel\Generator\Config\GeneratorConfig;
+use Propel\Generator\Manager\SqlManager;
+use Propel\Generator\Util\Filesystem;
+
+/**
+ * @author William Durand <william.durand1@gmail.com>
+ */
+class InsertSql extends AbstractCommand
+{
+    const DEFAULT_OUTPUT_DIRECTORY  = 'generated-sql';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('sql:insert')
+            ->setDescription('Insert SQL')
+            ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = new SqlManager();
+        $manager->setConnections(array());
+        $manager->setLoggerClosure(function($message) use ($input, $output) {
+            if ($input->getOption('verbose')) {
+                $output->writeln($message);
+            }
+        });
+        $manager->setWorkingDirectory($input->getOption('output-dir'));
+
+        $manager->insertSql();
+    }
+}
