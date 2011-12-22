03332e3410be78f73f3983da53b7a864c20323b7
William DURAND <william.durand1@gmail.com>
2011-12-22 22:37:51 +0100

    [Command] Added 'migration:up' command

---
+++ src/Propel/Generator/Command/MigrationUp.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Propel\Generator\Command;
+
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+
+use Propel\Generator\Config\GeneratorConfig;
+use Propel\Generator\Manager\MigrationManager;
+use Propel\Generator\Util\Filesystem;
+
+/**
+ * @author William Durand <william.durand1@gmail.com>
+ */
+class MigrationUp extends AbstractCommand
+{
+    const DEFAULT_OUTPUT_DIRECTORY  = 'generated-migrations';
+
+    const DEFAULT_MIGRATION_TABLE   = 'propel_migration';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->addOption('output-dir', null, InputOption::VALUE_REQUIRED,  'The output directory', self::DEFAULT_OUTPUT_DIRECTORY)
+            ->addOption('migration-table', null, InputOption::VALUE_REQUIRED,  'Migration table name', self::DEFAULT_MIGRATION_TABLE)
+            ->setName('migration:up')
+            ->setDescription('Execute migrations')
+            ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $generatorConfig = new GeneratorConfig(array(
+            'propel.platform.class' => $input->getOption('platform'),
+        ));
+
+        $filesystem = new Filesystem();
+        $filesystem->mkdir($input->getOption('output-dir'));
+
+        $manager = new MigrationManager();
+        $manager->setGeneratorConfig($generatorConfig);
+
+        // /!\
+        // $manager->setConnections($this->getGeneratorConfig()->getBuildConnections());
+
+        $manager->setMigrationTable($input->getOption('migration-table'));
+        $manager->setWorkingDirectory($input->getOption('output-dir'));
+
+        if (!$nextMigrationTimestamp = $manager->getFirstUpMigrationTimestamp()) {
+            $output->writeln('All migrations were already executed - nothing to migrate.');
+
+            return false;
+        }
+        $output->writeln(sprintf(
+            'Executing migration %s up',
+            $manager->getMigrationClassName($nextMigrationTimestamp)
+        ));
+
+        $migration = $manager->getMigrationObject($nextMigrationTimestamp);
+        if (false === $migration->preUp($manager)) {
+            $output->writeln('<error>preUp() returned false. Aborting migration.</error>');
+
+            return false;
+        }
+
+        foreach ($migration->getUpSQL() as $datasource => $sql) {
+            $connection = $manager->getConnection($datasource);
+
+            if ($this->getOption('verbose')) {
+                $output->writeln(sprintf(
+                    'Connecting to database "%s" using DSN "%s"',
+                    $datasource,
+                    $connection['dsn']
+                ));
+            }
+
+            $pdo = $manager->getPdoConnection($datasource);
+            $res = 0;
+            $statements = SqlParser::parseString($sql);
+            foreach ($statements as $statement) {
+                try {
+                    if ($this->getOption('verbose')) {
+                        $output->writeln(sprintf('Executing statement "%s"', $statement));
+                    }
+
+                    $stmt = $pdo->prepare($statement);
+                    $stmt->execute();
+                    $res++;
+                } catch (PDOException $e) {
+                    $output->writeln(sprintf('<error>Failed to execute SQL "%s". Aborting migration.</error>', $statement));
+
+                    return false;
+                }
+            }
+            if (!$res) {
+                $output->writeln('No statement was executed. The version was not updated.');
+                $output->writeln(sprintf(
+                    'Please review the code in "%s"',
+                    $manager->getMigrationDir() . DIRECTORY_SEPARATOR . $manager->getMigrationClassName($nextMigrationTimestamp)
+                ));
+                $output->writeln('<error>Migration aborted</error>', Project::MSG_ERR);
+
+                return false;
+            }
+            $output->writeln(sprintf(
+                '%d of %d SQL statements executed successfully on datasource "%s"',
+                $res,
+                count($statements),
+                $datasource
+            ));
+            $manager->updateLatestMigrationTimestamp($datasource, $nextMigrationTimestamp);
+            if ($this->getOption('verbose')) {
+                $output->writeln(sprintf(
+                    'Updated latest migration date to %d for datasource "%s"',
+                    $nextMigrationTimestamp,
+                    $datasource
+                ));
+            }
+        }
+
+        $migration->postUp($manager);
+
+        if ($timestamps = $manager->getValidMigrationTimestamps()) {
+            $output->writeln(sprintf(
+                'Migration complete. %d migrations left to execute.',
+                count($timestamps)
+            ));
+        } else {
+            $output->writeln('Migration complete. No further migration to execute.');
+        }
+    }
+}
