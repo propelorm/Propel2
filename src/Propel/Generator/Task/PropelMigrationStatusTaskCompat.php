4151f2f54498aa3d8c2d5b2c49d19268bbc22546
William DURAND <william.durand1@gmail.com>
2011-12-23 11:43:43 +0100

    [Generator] Removed no more used task (migration status)

--- src/Propel/Generator/Task/PropelMigrationStatusTaskCompat.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!class_exists('\Propel\Generator\Task\BasePropelMigrationTask')) {
-    require_once __DIR__ . '/../../../../autoload.php.dist';
-}
-
-class PropelMigrationStatusTaskCompat extends \Propel\Generator\Task\PropelMigrationStatusTask
-{
-}
