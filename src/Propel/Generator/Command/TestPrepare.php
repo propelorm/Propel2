765c5bb8b43cf0e573739b8b46c3e8a880200907
William DURAND <william.durand1@gmail.com>
2012-03-06 22:10:45 +0100

    [Generator] Refactored command 'test:prepare'

--- src/Propel/Generator/Command/TestPrepare.php
+++ src/Propel/Generator/Command/TestPrepare.php
@@ -17,12 +17,17 @@ class TestPrepare extends Command
     /**
      * @var string
      */
+    const FIXTURES_DIR      = 'tests/Fixtures';
+
+    /**
+     * @var string
+     */
     const DEFAULT_VENDOR    = 'mysql';
 
     /**
      * @var string
      */
-    const DEFAULT_DB_SERVER = '127.0.0.1';
+    const DEFAULT_DSN       = 'mysql:host=127.0.0.1;dbname=test';
 
     /**
      * @var string
@@ -70,14 +75,13 @@ class TestPrepare extends Command
     {
         $this
             ->setDefinition(array(
-                new InputOption('vendor', null, InputOption::VALUE_REQUIRED, 'The database vendor', self::DEFAULT_VENDOR),
-                new InputOption('server', 's', InputOption::VALUE_REQUIRED, 'The database server', self::DEFAULT_DB_SERVER),
-                new InputOption('user', 'u', InputOption::VALUE_REQUIRED, 'The database user', self::DEFAULT_DB_USER),
-                new InputOption('password', 'p', InputOption::VALUE_REQUIRED, 'The database password', self::DEFAULT_DB_PASSWD),
-                new InputOption('fixtures-dir', null, InputOption::VALUE_REQUIRED, 'A fixture directory to build', null),
+                new InputOption('vendor',       null, InputOption::VALUE_REQUIRED, 'The database vendor', self::DEFAULT_VENDOR),
+                new InputOption('dsn',          null, InputOption::VALUE_OPTIONAL, 'The data source name', self::DEFAULT_DSN),
+                new InputOption('user',          'u', InputOption::VALUE_REQUIRED, 'The database user', self::DEFAULT_DB_USER),
+                new InputOption('password',      'p', InputOption::VALUE_REQUIRED, 'The database password', self::DEFAULT_DB_PASSWD),
             ))
             ->setName('test:prepare')
-            ->setDescription('Prepare unit tests by building fixtures')
+            ->setDescription('Prepare the Propel test suite by building fixtures')
             ;
     }
 
@@ -86,12 +90,8 @@ class TestPrepare extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (null !== $fixturesDir = $input->getOption('fixtures-dir')) {
-            $this->buildFixtures($fixturesDir, $input, $output);
-        } else {
-            foreach ($this->fixturesDirs as $fixturesDir) {
-                $this->buildFixtures('tests/Fixtures/'.$fixturesDir, $input, $output);
-            }
+        foreach ($this->fixturesDirs as $fixturesDir) {
+            $this->buildFixtures(sprintf('%s/%s', self::FIXTURES_DIR, $fixturesDir), $input, $output);
         }
     }
 
@@ -119,9 +119,9 @@ class TestPrepare extends Command
             if (is_file($sourceFile)) {
                 $content = file_get_contents($sourceFile);
 
-                $content = str_replace('##DATABASE_VENDOR##', $input->getOption('vendor'), $content);
-                $content = str_replace('##DATABASE_SERVER##', $input->getOption('server'), $content);
-                $content = str_replace('##DATABASE_USER##', $input->getOption('user'), $content);
+                $content = str_replace('##DATABASE_VENDOR##',   $input->getOption('vendor'), $content);
+                $content = str_replace('##DATABASE_URL##',      $input->getOption('dsn'), $content);
+                $content = str_replace('##DATABASE_USER##',     $input->getOption('user'), $content);
                 $content = str_replace('##DATABASE_PASSWORD##', $input->getOption('password'), $content);
 
                 file_put_contents($targetFile, $content);
@@ -132,32 +132,30 @@ class TestPrepare extends Command
             }
         }
 
-        if (is_file('schema.xml')) {
-            shell_exec(sprintf('"%s" convert-conf', $this->propelgen));
+        shell_exec(sprintf('"%s" convert-conf', $this->propelgen));
 
-            // use new commands
-            $in = new ArrayInput(array(
-                'command'	    => 'sql:build',
-                '--input-dir'   => '.',
-                '--output-dir'  => 'build/sql/',
-                '--platform'    => ucfirst($input->getOption('vendor')) . 'Platform',
-                '--verbose'		=> $input->getOption('verbose'),
-            ));
+        // use new commands
+        $in = new ArrayInput(array(
+            'command'	    => 'sql:build',
+            '--input-dir'   => '.',
+            '--output-dir'  => 'build/sql/',
+            '--platform'    => ucfirst($input->getOption('vendor')) . 'Platform',
+            '--verbose'		=> $input->getOption('verbose'),
+        ));
 
-            $command = $this->getApplication()->find('sql:build');
-            $command->run($in, $output);
+        $command = $this->getApplication()->find('sql:build');
+        $command->run($in, $output);
 
-            $in = new ArrayInput(array(
-                'command'       => 'model:build',
-                '--input-dir'   => '.',
-                '--output-dir'  => 'build/classes/',
-                '--platform'    => ucfirst($input->getOption('vendor')) . 'Platform',
-                '--verbose'		=> $input->getOption('verbose'),
-            ));
+        $in = new ArrayInput(array(
+            'command'       => 'model:build',
+            '--input-dir'   => '.',
+            '--output-dir'  => 'build/classes/',
+            '--platform'    => ucfirst($input->getOption('vendor')) . 'Platform',
+            '--verbose'		=> $input->getOption('verbose'),
+        ));
 
-            $command = $this->getApplication()->find('model:build');
-            $command->run($in, $output);
-        }
+        $command = $this->getApplication()->find('model:build');
+        $command->run($in, $output);
 
         shell_exec(sprintf('"%s" insert-sql', $this->propelgen));
 
