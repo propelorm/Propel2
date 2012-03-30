cdcab7d61a5132b85ac0298828faec50597c8a60
Kévin Gomez Pinto <kevin_gomez@carpe-hora.com>
2012-03-30 16:26:29 +0200

    Fix: port of the PR #319 from Propel

--- tests/Propel/Tests/Generator/Behavior/Versionable/VersionableBehaviorObjectBuilderModifierTest.php
+++ tests/Propel/Tests/Generator/Behavior/Versionable/VersionableBehaviorObjectBuilderModifierTest.php
@@ -584,6 +584,7 @@ EOF;
     public function testVersionComment()
     {
         $o = new \VersionableBehaviorTest4();
+
         $o->setVersionComment('Because you deserve it');
         $o->save();
         $version = \VersionableBehaviorTest4VersionQuery::create()
@@ -639,6 +640,69 @@ EOF;
         $this->assertEquals(123, $versions[0]->getBar());
         $this->assertEquals(2, $versions[1]->getVersion());
         $this->assertEquals(456, $versions[1]->getBar());
+    }
+
+    public function testGetLastVersions()
+    {
+        $o = new \VersionableBehaviorTest1();
+        $versions = $o->getAllVersions();
+        $this->assertTrue($versions->isEmpty());
+        $o->setBar(123); // version 1
+        $o->save();
+        $o->setBar(456); // version 2
+        $o->save();
+        $o->setBar(789); // version 3
+        $o->save();
+        $o->setBar(101112); // version 4
+        $o->save();
+
+        $versions = $o->getLastVersions();
+        $this->assertTrue($versions instanceof ObjectCollection);
+        $this->assertEquals(4, $versions->count());
+        $this->assertEquals(4, $versions[0]->getVersion());
+        $this->assertEquals(101112, $versions[0]->getBar());
+        $this->assertEquals(3, $versions[1]->getVersion());
+        $this->assertEquals(789, $versions[1]->getBar());
+        $this->assertEquals(2, $versions[2]->getVersion());
+        $this->assertEquals(456, $versions[2]->getBar());
+        $this->assertEquals(1, $versions[3]->getVersion());
+        $this->assertEquals(123, $versions[3]->getBar());
+
+        $versions = $o->getLastVersions(2);
+        $this->assertTrue($versions instanceof ObjectCollection);
+        $this->assertEquals(2, $versions->count());
+        $this->assertEquals(4, $versions[0]->getVersion());
+        $this->assertEquals(101112, $versions[0]->getBar());
+        $this->assertEquals(3, $versions[1]->getVersion());
+        $this->assertEquals(789, $versions[1]->getBar());
+    }
+
+    public function testCompareVersion()
+    {
+        $o = new \VersionableBehaviorTest4();
+        $versions = $o->getAllVersions();
+        $this->assertTrue($versions->isEmpty());
+        $o->setBar(123); // version 1
+        $o->save();
+        $o->setBar(456); // version 2
+        $o->save();
+        $o->setBar(789); // version 3
+        $o->setVersionComment('Foo');
+        $o->save();
+        $diff = $o->compareVersion(3); // $o is in version 3
+        $expected = array();
+        $this->assertEquals($expected, $diff);
+        $diff = $o->compareVersion(2);
+        $expected = array(
+            'Bar' => array(2 => 456, 3 => 789),
+        );
+        $this->assertEquals($expected, $diff);
+
+        $diff = $o->compareVersion(1);
+        $expected = array(
+            'Bar' => array(1 => 123, 3 => 789),
+        );
+        $this->assertEquals($expected, $diff);
     }
 
     public function testCompareVersions()
