cdd36998238407e4ac30e143d5e57c883e666ed3
Pierre Tachoire <pierre.tachoire@gmail.com>
2012-03-30 16:29:46 +0200

    Port PR #326 from Propel 1.6

---
+++ tests/Propel/Tests/Generator/Reverse/MssqlSchemaParserTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Propel\Tests\Generator\Reverse;
+
+use Propel\Generator\Reverse\MssqlSchemaParser;
+
+/**
+ * Tests for Mssql database schema parser.
+ *
+ * @author      Pierre Tachoire
+ */
+class MssqlSchemaParserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCleanDelimitedIdentifiers()
+    {
+        $parser = new TestableMssqlSchemaParser(null);
+
+        $expected = 'this is a tablename';
+
+        $tested = $parser->cleanDelimitedIdentifiers('\''.$expected.'\'');
+        $this->assertEquals($expected, $tested);
+
+        $tested = $parser->cleanDelimitedIdentifiers('\''.$expected);
+        $this->assertEquals('\''.$expected, $tested);
+
+        $tested = $parser->cleanDelimitedIdentifiers($expected.'\'');
+        $this->assertEquals($expected.'\'', $tested);
+
+        $expected = 'this is a tabl\'ename';
+
+        $tested = $parser->cleanDelimitedIdentifiers('\''.$expected.'\'');
+        $this->assertEquals($expected, $tested);
+
+        $tested = $parser->cleanDelimitedIdentifiers('\''.$expected);
+        $this->assertEquals('\''.$expected, $tested);
+
+        $tested = $parser->cleanDelimitedIdentifiers($expected.'\'');
+        $this->assertEquals($expected.'\'', $tested);
+
+        $expected = 'this is a\'tabl\'ename';
+
+        $tested = $parser->cleanDelimitedIdentifiers('\''.$expected.'\'');
+        $this->assertEquals($expected, $tested);
+
+        $tested = $parser->cleanDelimitedIdentifiers('\''.$expected);
+        $this->assertEquals('\''.$expected, $tested);
+
+        $tested = $parser->cleanDelimitedIdentifiers($expected.'\'');
+        $this->assertEquals($expected.'\'', $tested);
+
+    }
+}
+
+class TestableMssqlSchemaParser extends MssqlSchemaParser
+{
+    public function cleanDelimitedIdentifiers($identifier)
+    {
+        return parent::cleanDelimitedIdentifiers($identifier);
+    }
+}
