831c23fb6b2b37371f38a650ac47c93140104917
William DURAND <william.durand1@gmail.com>
2012-04-14 17:58:26 +0200

    Added Propel 1.6 default pluralizer. Closes #103

---
+++ src/Propel/Common/Pluralizer/SimpleEnglishPluralizer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Propel\Common\Pluralizer;
+
+/**
+ * The Propel 1.6 default English pluralizer class
+ * for compatibility only.
+ *
+ * @author     Hans Lellelid <hans@xmpl.org>
+ */
+class SimpleEnglishPluralizer implements PluralizerInterface
+{
+    /**
+     * Generate a plural name based on the passed in root.
+     *
+     * @param      string $root The root that needs to be pluralized (e.g. Author)
+     * @return     string The plural form of $root (e.g. Authors).
+     */
+    public function getPluralForm($root)
+    {
+        return $root . 's';
+    }
+}
