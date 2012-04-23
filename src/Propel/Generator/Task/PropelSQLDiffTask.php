51bbbeb6f9418e07536589e39814f585e8eb82d0
kphoen <geek63@gmail.com>
2012-04-23 22:03:22 +0200

    Fix: port PR #298 from Propel
    See https://github.com/propelorm/Propel/pull/298/files

--- src/Propel/Generator/Task/PropelSQLDiffTask.php
+++ src/Propel/Generator/Task/PropelSQLDiffTask.php
@@ -107,6 +107,7 @@ class PropelSQLDiffTask extends AbstractPropelDataModelTask
         $manager = new PropelMigrationManager();
         $manager->setConnections($connections);
         $manager->setMigrationDir($this->getOutputDirectory());
+        $manager->setMigrationTable($this->getGeneratorConfig()->getBuildProperty('migrationTable'));
 
         if ($manager->hasPendingMigrations()) {
             throw new Exception('Uncommitted migrations have been found ; you should either execute or delete them before rerunning the \'diff\' task');
