460d6da3d027d6682b4c8d67f7b48d7b9f5db8a1
Francois Zaninotto <fzaninotto@gmail.com>
2012-04-28 10:34:17 +0200

    Add runtime configuration converters.
    This prepares the refactoring of the runtime configuration cache, and
    allows the transformation of a configuration array into a php
    configuration (using the ServiceContainer) to occur at buildtime rather
    than runtime.
    
    Not to mention that it clarifies the runtime configuration settings, and
    encourages the direct use of PHP configuration rather than XML.
    
    The commit also fixes a bad parsing of the <slaves> element. The
    documentation explains that <slave> should be under <datasource>, while
    the configuration transformation done in Propel::setConfiguration()
    expected the <slave> element to be under <connection>.
    
    Refs 181.

---
+++ src/Propel/Generator/Config/ArrayToPhpConverter.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Propel\Generator\Config;
+
+/**
+ * Runtime configuration converter
+ * From array to PHP string
+ */
+class ArrayToPhpConverter
+{
+    /**
+     * Create a PHP configuration from an array
+     *
+     * @param Array $configuration The array configuration
+     *
+     * @return String
+     */
+    public static function convert($c)
+    {
+      $conf = "\$serviceContainer = \Propel\Runtime\Propel::getServiceContainer();";
+      // set datasources
+      if (isset($c['datasources'])) {
+            foreach ($c['datasources'] as $name => $params) {
+                if (!is_array($params)) {
+                    continue;
+                }
+                // set adapters
+                if (isset($params['adapter'])) {
+                    $conf .= "
+\$serviceContainer->setAdapterClass('{$name}', '{$params['adapter']}');";
+                }
+                // set connection settings
+                if (isset($params['slaves']) || isset($params['connection'])) {
+                    if (isset($params['slaves'])) {
+                        $conf .= "
+\$manager = new \Propel\Runtime\Connection\ConnectionManagerMasterSlave();
+\$manager->setReadConfiguration(" . var_export($params['slaves'], true). ");";
+                        unset($params['slaves']);
+                    } else {
+                        $conf .= "
+\$manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();";
+                    }
+                    if (isset($params['connection'])) {
+                      $conf .= "
+\$manager->setWriteConfiguration(". var_export($params['connection'], true) . ");";
+                    }
+                    $conf .= "
+\$manager->setName('{$name}');
+\$serviceContainer->setConnectionManager('{$name}', \$manager);";
+                }
+            }
+            // set default datasource
+            if (isset($c['datasources']['default'])) {
+                $defaultDatasource = $c['datasources']['default'];
+            } elseif (isset($c['datasources']) && is_array($c['datasources'])) {
+                // fallback to the first datasource
+                $datasourceNames = array_keys($c['datasources']);
+                $defaultDatasource = $datasourceNames[0];
+            }
+            $conf .= "
+\$serviceContainer->setDefaultDatasource('{$defaultDatasource}');";
+        }
+        // set profiler
+        if (isset($c['profiler'])) {
+            $profilerConf = $c['profiler'];
+            if (isset($profilerConf['class'])) {
+                $conf .= "
+\$serviceContainer->setProfilerClass('{$profilerConf['class']}');";
+                unset($profilerConf['class']);
+            }
+            if ($profilerConf) {
+                $conf .= "
+\$serviceContainer->setProfilerConfiguration(" . var_export($profilerConf, true) . ");";
+            }
+            unset($c['profiler']);
+        }
+        // set logger
+        if (isset($c['log'])) {
+            $loggerConfiguration = $c['log'];
+            $name = $loggerConfiguration['name'];
+            unset($loggerConfiguration['name']);
+            $conf .= "
+\$serviceContainer->setLoggerConfiguration('{$name}', " . var_export($loggerConfiguration, true) . ");";
+            unset($c['log']);
+        }
+        return $conf;
+    }
+
+}
---
+++ tests/Propel/Tests/Generator/Config/ArrayToPhpConverterTest.php
@@ -0,0 +1,251 @@
+<?php
+
+/**
+ * This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Propel\Tests\Generator\Config;
+
+use Propel\Generator\Config\ArrayToPhpConverter;
+
+class ArrayToPhpConverterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConvertConvertsSimpleDatasourceSection()
+    {
+        $conf = array('datasources' => array(
+          'bookstore' => array(
+            'adapter' => 'mysql',
+            'connection' => array(
+              'classname' => 'DebugPDO',
+              'dsn' => 'mysql:host=localhost;dbname=bookstore',
+              'user' => 'testuser',
+              'password' => 'password',
+              'options' =>  array('ATTR_PERSISTENT' => false),
+              'attributes' => array('ATTR_EMULATE_PREPARES' => true),
+            ),
+          ),
+          'default' => 'bookstore',
+        ));
+        $expected = <<<'EOF'
+$serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
+$serviceContainer->setAdapterClass('bookstore', 'mysql');
+$manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();
+$manager->setWriteConfiguration(array (
+  'classname' => 'DebugPDO',
+  'dsn' => 'mysql:host=localhost;dbname=bookstore',
+  'user' => 'testuser',
+  'password' => 'password',
+  'options' => 
+  array (
+    'ATTR_PERSISTENT' => false,
+  ),
+  'attributes' => 
+  array (
+    'ATTR_EMULATE_PREPARES' => true,
+  ),
+));
+$manager->setName('bookstore');
+$serviceContainer->setConnectionManager('bookstore', $manager);
+$serviceContainer->setDefaultDatasource('bookstore');
+EOF;
+        $this->assertEquals($expected, ArrayToPhpConverter::convert($conf));
+    }
+
+    public function testConvertConvertsMasterSlaveDatasourceSection()
+    {
+        $conf = array('datasources' => array(
+          'bookstore-cms' => array(
+            'adapter' => 'mysql',
+            'connection' => array('dsn' => 'mysql:host=localhost;dbname=bookstore'),
+            'slaves' => array(
+              'connection' => array(
+                array('dsn' => 'mysql:host=slave-server1; dbname=bookstore'),
+                array('dsn' => 'mysql:host=slave-server2; dbname=bookstore'),
+              ),
+            ),
+          ),
+        ));
+        $expected = <<<'EOF'
+$serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
+$serviceContainer->setAdapterClass('bookstore-cms', 'mysql');
+$manager = new \Propel\Runtime\Connection\ConnectionManagerMasterSlave();
+$manager->setReadConfiguration(array (
+  'connection' => 
+  array (
+    0 => 
+    array (
+      'dsn' => 'mysql:host=slave-server1; dbname=bookstore',
+    ),
+    1 => 
+    array (
+      'dsn' => 'mysql:host=slave-server2; dbname=bookstore',
+    ),
+  ),
+));
+$manager->setWriteConfiguration(array (
+  'dsn' => 'mysql:host=localhost;dbname=bookstore',
+));
+$manager->setName('bookstore-cms');
+$serviceContainer->setConnectionManager('bookstore-cms', $manager);
+$serviceContainer->setDefaultDatasource('bookstore-cms');
+EOF;
+        $this->assertEquals($expected, ArrayToPhpConverter::convert($conf));
+    }
+
+    public function testConvertConvertsProfilerSection()
+    {
+        $conf = array('profiler' => array(
+            'class' => '\Runtime\Runtime\Util\Profiler',
+            'slowTreshold' => 0.2,
+            'details' => array(
+                'time' => array('name' => 'Time', 'precision' => 3, 'pad' => '8'),
+                'mem' => array('name' => 'Memory', 'precision' => 3, 'pad' => '8'),
+            ),
+            'innerGlue' => ': ',
+            'outerGlue' => ' | '
+        ));
+        $expected = <<<'EOF'
+$serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
+$serviceContainer->setProfilerClass('\Runtime\Runtime\Util\Profiler');
+$serviceContainer->setProfilerConfiguration(array (
+  'slowTreshold' => 0.2,
+  'details' => 
+  array (
+    'time' => 
+    array (
+      'name' => 'Time',
+      'precision' => 3,
+      'pad' => '8',
+    ),
+    'mem' => 
+    array (
+      'name' => 'Memory',
+      'precision' => 3,
+      'pad' => '8',
+    ),
+  ),
+  'innerGlue' => ': ',
+  'outerGlue' => ' | ',
+));
+EOF;
+        $this->assertEquals($expected, ArrayToPhpConverter::convert($conf));
+    }
+
+    public function testConvertConvertsLogSection()
+    {
+        $conf = array('log' => array(
+            'type' => 'file',
+            'name' => '/path/to/propel.log',
+            'ident' => 'propel-bookstore',
+            'level' => 7
+        ));
+        $expected = <<<'EOF'
+$serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
+$serviceContainer->setLoggerConfiguration('/path/to/propel.log', array (
+  'type' => 'file',
+  'ident' => 'propel-bookstore',
+  'level' => 7,
+));
+EOF;
+        $this->assertEquals($expected, ArrayToPhpConverter::convert($conf));
+    }
+
+    public function testConvertConvertsCompleteConfiguration()
+    {
+        $conf = array(
+          'log' => array(
+            'type' => 'file',
+            'name' => '/path/to/propel.log',
+            'ident' => 'propel-bookstore',
+            'level' => 7
+          ),
+          'datasources' => array(
+            'bookstore' => array(
+              'adapter' => 'mysql',
+              'connection' => array(
+                'classname' => '\\Propel\\Runtime\\Connection\\DebugPDO',
+                'dsn' => 'mysql:host=127.0.0.1;dbname=test',
+                'user' => 'root',
+                'password' => '',
+                'options' => array(
+                  'ATTR_PERSISTENT' => false,
+                ),
+                'attributes' => array(
+                  'ATTR_EMULATE_PREPARES' => true,
+                ),
+                'settings' => array(
+                  'charset' => 'utf8',
+                ),
+              ),
+            ),
+            'bookstore-cms' => array(
+              'adapter' => 'mysql',
+              'connection' => array('dsn' => 'mysql:host=localhost;dbname=bookstore'),
+              'slaves' => array(
+                'connection' => array(
+                  array('dsn' => 'mysql:host=slave-server1; dbname=bookstore'),
+                  array('dsn' => 'mysql:host=slave-server2; dbname=bookstore'),
+                ),
+              ),
+            ),
+            'default' => 'bookstore',
+          ),
+        );
+        $expected = <<<'EOF'
+$serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
+$serviceContainer->setAdapterClass('bookstore', 'mysql');
+$manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();
+$manager->setWriteConfiguration(array (
+  'classname' => '\\Propel\\Runtime\\Connection\\DebugPDO',
+  'dsn' => 'mysql:host=127.0.0.1;dbname=test',
+  'user' => 'root',
+  'password' => '',
+  'options' => 
+  array (
+    'ATTR_PERSISTENT' => false,
+  ),
+  'attributes' => 
+  array (
+    'ATTR_EMULATE_PREPARES' => true,
+  ),
+  'settings' => 
+  array (
+    'charset' => 'utf8',
+  ),
+));
+$manager->setName('bookstore');
+$serviceContainer->setConnectionManager('bookstore', $manager);
+$serviceContainer->setAdapterClass('bookstore-cms', 'mysql');
+$manager = new \Propel\Runtime\Connection\ConnectionManagerMasterSlave();
+$manager->setReadConfiguration(array (
+  'connection' => 
+  array (
+    0 => 
+    array (
+      'dsn' => 'mysql:host=slave-server1; dbname=bookstore',
+    ),
+    1 => 
+    array (
+      'dsn' => 'mysql:host=slave-server2; dbname=bookstore',
+    ),
+  ),
+));
+$manager->setWriteConfiguration(array (
+  'dsn' => 'mysql:host=localhost;dbname=bookstore',
+));
+$manager->setName('bookstore-cms');
+$serviceContainer->setConnectionManager('bookstore-cms', $manager);
+$serviceContainer->setDefaultDatasource('bookstore');
+$serviceContainer->setLoggerConfiguration('/path/to/propel.log', array (
+  'type' => 'file',
+  'ident' => 'propel-bookstore',
+  'level' => 7,
+));
+EOF;
+        $this->assertEquals($expected, ArrayToPhpConverter::convert($conf));
+    }
+}
