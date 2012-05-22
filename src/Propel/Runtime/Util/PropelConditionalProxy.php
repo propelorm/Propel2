411a0eaabc74d571c69d6ba79236780a66bf2c95
Hugo Hamon <hugo.hamon@sensio.com>
2012-05-22 19:47:07 +0200

    [Propel] removed extra spaces between @license tag and the license name.

--- src/Propel/Runtime/Util/PropelConditionalProxy.php
+++ src/Propel/Runtime/Util/PropelConditionalProxy.php
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @license    MIT License
+ * @license MIT License
  */
 
 namespace Propel\Runtime\Util;
