c19389dc0d9caf1eee1db8f6dffd27016346c341
Robin Dupret <robin.dupret@gmail.com>
2012-06-14 19:36:56 +0200

    Missing revert of the CS fix

--- tests/Propel/Tests/Generator/Behavior/AlternativeCodingStandards/AlternativeCodingStandardsBehaviorTest.php
+++ tests/Propel/Tests/Generator/Behavior/AlternativeCodingStandards/AlternativeCodingStandardsBehaviorTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- *	$Id: TimestampableBehaviorTest.php 2035 2010-11-14 17:54:27Z francois $
+ *  $Id: TimestampableBehaviorTest.php 2035 2010-11-14 17:54:27Z francois $
  * This file is part of the Propel package.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -29,13 +29,17 @@ class AlternativeCodingStandardsBehaviorTest extends \PHPUnit_Framework_TestCase
 {
 }"),
             array("if (true) {
-}", "if (true) {
+}", "if (true)
+{
 }"),
             array("} else {
-}", "} else {
+}", "}
+else
+{
 }"),
             array("foreach (\$i as \$j) {
-}", "foreach (\$i as \$j) {
+}", "foreach (\$i as \$j)
+{
 }"),
         );
     }
@@ -51,8 +55,7 @@ class AlternativeCodingStandardsBehaviorTest extends \PHPUnit_Framework_TestCase
     }
 }
 
-class TestableAlternativeCodingStandardsBehavior extends AlternativeCodingStandardsBehavior
-{
+class TestableAlternativeCodingStandardsBehavior extends AlternativeCodingStandardsBehavior {
     public function filter(&$script)
     {
         return parent::filter($script);
