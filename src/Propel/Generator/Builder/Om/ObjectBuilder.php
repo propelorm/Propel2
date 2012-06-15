56ca280fdff68b15d5494a2c603eab538847ed10
Fabien Pomerol <fabien.pomerol@gmail.com>
2012-06-15 15:42:21 +0200

    Port PR propelorm/Propel#204

--- src/Propel/Generator/Builder/Om/ObjectBuilder.php
+++ src/Propel/Generator/Builder/Om/ObjectBuilder.php
@@ -221,12 +221,13 @@ class ObjectBuilder extends AbstractObjectBuilder
  */
 abstract class ".$this->getUnqualifiedClassName()." extends ".$parentClass." ";
 
-        $interface = ClassTools::getInterface($table);
-        if ($interface) {
-            $script .= " implements " . ClassTools::classname($interface);
-        }
-        if ($this->getTable()->getInterface()) {
-            $this->declareClassFromBuilder($this->getInterfaceBuilder());
+        if ($interface = $this->getTable()->getInterface()) {
+            $script .= "implements " . "Child" . ClassTools::classname($interface);
+            if ($interface !== ClassTools::classname($interface)) {
+               $this->declareClass($interface);
+            } else {
+                $this->declareClassFromBuilder($this->getInterfaceBuilder());
+            }
         }
 
         $script .= "
