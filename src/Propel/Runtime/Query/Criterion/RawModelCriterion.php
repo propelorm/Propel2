ab1ffa34d35e6db3839f2d6d489b03387aa60562
Francois Zaninotto <fzaninotto@gmail.com>
2012-07-28 20:32:18 +0200

    Final cleanups and optimizations in ModelCriterion logic
    Refs #41

--- src/Propel/Runtime/Query/Criterion/RawModelCriterion.php
+++ src/Propel/Runtime/Query/Criterion/RawModelCriterion.php
@@ -30,12 +30,12 @@ class RawModelCriterion extends AbstractModelCriterion
     /**
      * Create a new instance.
      *
-     * @param Criteria  $outer The outer class (this is an "inner" class).
-     * @param string    $clause A simple pseudo-SQL clause, e.g. 'foo.BAR LIKE ?'
-     * @param ColumnMap $column A Column object to help escaping the value
+     * @param Criteria  $outer      The outer class (this is an "inner" class).
+     * @param string    $clause     A simple pseudo-SQL clause, e.g. 'foo.BAR LIKE ?'
+     * @param ColumnMap $column     A Column object to help escaping the value
      * @param mixed     $value
      * @param string    $tableAlias optional table alias
-     * @param string    $type   A PDO type constant, e.g. PDO::PARAM_STR
+     * @param string    $type       A PDO type constant, e.g. PDO::PARAM_STR
      */
     public function __construct(Criteria $outer, $clause, $column, $value = null, $tableAlias = null, $type = PDO::PARAM_STR)
     {
