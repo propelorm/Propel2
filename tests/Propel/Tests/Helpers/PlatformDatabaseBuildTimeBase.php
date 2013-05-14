bbb8544d95aa4b0911425365ecc131a8114436a9
marcj <marc@kryn.org>
2013-05-15 01:54:22 +0200

    Ref #381. Ported propelorm/propel#618

---
+++ tests/Propel/Tests/Helpers/PlatformDatabaseBuildTimeBase.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Propel\Tests\Helpers;
+
+use Propel\Generator\Config\QuickGeneratorConfig;
+use Propel\Generator\Model\Database;
+use Propel\Generator\Model\Diff\DatabaseComparator;
+use Propel\Generator\Platform\MysqlPlatform;
+use Propel\Generator\Reverse\MysqlSchemaParser;
+use Propel\Generator\Util\SqlParser;
+use Propel\Runtime\Propel;
+
+class PlatformDatabaseBuildTimeBase extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Database
+     */
+    public $database;
+
+    /**
+     * @var MysqlSchemaParser
+     */
+    public $parser;
+
+    /**
+     * @var MysqlPlatform
+     */
+    public $platform;
+
+    /**
+     * @var PDO
+     */
+    public $con;
+
+    protected function setUp()
+    {
+        include(__DIR__ . '/../../../Fixtures/reverse/mysql/build/conf/reverse-bookstore-conf.php');
+
+        $this->con = Propel::getConnection('reverse-bookstore');
+
+        $this->parser   = new MysqlSchemaParser($this->con);
+        $this->platform = new MysqlPlatform();
+
+        $this->parser->setGeneratorConfig(new QuickGeneratorConfig());
+        $this->parser->setPlatform($this->platform);
+        parent::setUp();
+    }
+
+    public function readDatabase()
+    {
+        $this->database = new Database();
+        $this->database->setPlatform($this->platform);
+        $this->parser->parse($this->database);
+    }
+
+    /**
+     * Detects the differences between current connected database and $pDatabase
+     * and updates the schema. This does not DROP tables.
+     *
+     * @param Database $pDatabase
+     */
+    public function updateSchema($pDatabase)
+    {
+        $diff = DatabaseComparator::computeDiff($this->database, $pDatabase);
+        $sql = $this->database->getPlatform()->getModifyDatabaseDDL($diff);
+
+        $statements = SqlParser::parseString($sql);
+        foreach ($statements as $statement) {
+            if (strpos($statement, 'DROP') === 0) {
+                // drop statements cause errors since the table doesn't exist
+                continue;
+            }
+            $stmt = $this->con->prepare($statement);
+            $stmt->execute();
+        }
+    }
+}
