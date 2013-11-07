b01785256c80ab07cd3b87a4953c705d513e6598
Marc J. Schmidt <marc@kryn.org>
2013-11-07 22:51:46 +0100

    Fixed bug where tableMaps were not cached. #478 - Performance increase from 16,252,928bytes/3.284s =>  12,058,624bytes/2.170s

--- src/Propel/Runtime/Map/DatabaseMap.php
+++ src/Propel/Runtime/Map/DatabaseMap.php
@@ -92,7 +92,11 @@ class DatabaseMap
     {
         $table->setDatabaseMap($this);
         $this->tables[$table->getName()] = $table;
-        $this->tablesByPhpName[$table->getClassName()] = $table;
+        $phpName = $table->getClassName();
+        if ('\\' !== $phpName[0]) {
+            $phpName = '\\' . $phpName;
+        }
+        $this->tablesByPhpName[$phpName] = $table;
     }
 
     /**
@@ -171,6 +175,9 @@ class DatabaseMap
 
     public function getTableByPhpName($phpName)
     {
+        if ('\\' !== $phpName[0]) {
+            $phpName = '\\' . $phpName;
+        }
         if (isset($this->tablesByPhpName[$phpName])) {
             return $this->tablesByPhpName[$phpName];
         }
@@ -189,7 +196,6 @@ class DatabaseMap
                 return $this->tablesByPhpName[$phpName];
             }
 
-            $phpName = '\\'.$phpName;
             if (isset($this->tablesByPhpName[$phpName])) {
                 return $this->tablesByPhpName[$phpName];
             }
