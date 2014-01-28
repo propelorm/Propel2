86a4975a1a3a27a4929cf27b3c8d867e1130226f
Marc J. Schmidt <marc@kryn.org>
2014-01-28 23:49:27 +0100

    Added tests for GraphvizGenerateCommand

---
+++ tests/Propel/Tests/Command/GraphvizGenerateTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Propel\Tests\Command;
+
+use Propel\Generator\Command\GraphvizGenerateCommand;
+use Propel\Runtime\Propel;
+use Propel\Tests\TestCase;
+use Symfony\Component\Console\Application;
+
+class GraphvizGenerateTest extends TestCase
+{
+    public function testCommand()
+    {
+        $app = new Application('Propel', Propel::VERSION);
+        $command = new GraphvizGenerateCommand();
+        $app->add($command);
+
+        $outputDir = __DIR__.'/../../../graphviztest';
+
+        $input = new \Symfony\Component\Console\Input\ArrayInput(array(
+            'command' => 'graphviz:generate',
+            '--input-dir' => __DIR__ . '/../../../Fixtures/bookstore',
+            '--output-dir' => $outputDir,
+            '--verbose' => true
+        ));
+
+        $output = new \Symfony\Component\Console\Output\BufferedOutput();
+        $app->setAutoExit(false);
+        $result = $app->run($input, $output);
+
+        if (0 !== $result) {
+            echo $output->fetch();
+        }
+
+        $this->assertEquals(0, $result, 'graphviz:generate tests exited successfully');
+
+        $this->assertFileExists($outputDir.'/bookstore.schema.dot');
+        $content = file_get_contents($outputDir.'/bookstore.schema.dot');
+        $this->assertContains('digraph G {', $content);
+    }
+
+}
