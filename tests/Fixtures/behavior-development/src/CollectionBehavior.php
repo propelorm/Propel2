91510551e6351ca9ca346382257010583feb158e
gossi <github@gos.si>
2014-02-06 15:25:41 +0100

    Improvement to develop behaviors

---
+++ tests/Fixtures/behavior-development/src/CollectionBehavior.php
@@ -0,0 +1,15 @@
+<?php
+namespace Propel\Behavior\Collection;
+
+use Propel\Generator\Model\Behavior;
+
+class CollectionBehavior extends Behavior {
+
+    /* (non-PHPdoc)
+     * @see \Propel\Generator\Model\Behavior::modifyTable()
+    */
+    public function modifyTable() {
+        // behavior code
+    }
+
+}
