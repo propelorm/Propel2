504795f3084d45caf9d433b486316b03b996ccbb
tals56 <talstol1@gmail.com>
2014-05-03 20:42:37 +0300

    ActiveRecordObject->save() returns wrong results
    Hello,
    
    When I wish to update a certain ActiveRecord object in my database(without SELECTing it),
    I perform the following code:
    
    $myObj = new ActiveRecordObject();
    $myObj->setId(123); //The PK of the object which I wish to update
    $myObj->setField("NewFieldValue");
    $myObj->setNew(false);
    $affectedRows = $myObj->save(); //<-- Returns wrong results when such PK doesn't exist.
    
    The following edit fixes the problem.
    
    Thank you.

--- src/Propel/Generator/Builder/Om/ObjectBuilder.php
+++ src/Propel/Generator/Builder/Om/ObjectBuilder.php
@@ -5247,7 +5247,8 @@ abstract class ".$this->getUnqualifiedClassName().$parentClass." implements Acti
             if (\$this->isNew() || \$this->isModified()) {
                 // persist changes
                 if (\$this->isNew()) {
-                    \$this->doInsert(\$con);";
+                    \$this->doInsert(\$con);
+                    \$affectedRows += 1;";
         if ($reloadOnInsert) {
             $script .= "
                     if (!\$skipReload) {
@@ -5256,7 +5257,7 @@ abstract class ".$this->getUnqualifiedClassName().$parentClass." implements Acti
         }
         $script .= "
                 } else {
-                    \$this->doUpdate(\$con);";
+                    \$affectedRows += \$this->doUpdate(\$con);";
         if ($reloadOnUpdate) {
             $script .= "
                     if (!\$skipReload) {
@@ -5264,8 +5265,7 @@ abstract class ".$this->getUnqualifiedClassName().$parentClass." implements Acti
                     }";
         }
         $script .= "
-                }
-                \$affectedRows += 1;";
+                }";
 
         // We need to rewind any LOB columns
         foreach ($table->getColumns() as $col) {
