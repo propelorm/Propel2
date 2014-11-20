9bbc9f0c2602b7dbc10906631971fdbda5841145
Boban Acimovic <boban.acimovic@gmail.com>
2014-11-20 20:17:30 +0100

    Improved ConstantNameTest

--- tests/Propel/Tests/Generator/Model/ConstantNameTest.php
+++ tests/Propel/Tests/Generator/Model/ConstantNameTest.php
@@ -27,16 +27,19 @@ class ConstantNameTest extends TestCase
     {
         $schema = <<<XML
 <database name="constant_name_test" namespace="ConstantNameTest1">
-  <table name="radcheck" phpName="UserCheck">
+  <table name="radcheck" phpName="UserCheck1">
     <column name="id" type="INTEGER" sqlType="int(11) unsigned" primaryKey="true" autoIncrement="true" required="true"/>
     <column name="attribute" type="VARCHAR" size="64" required="true" inheritance="single">
-      <inheritance key="Expiration" class="UserCheckExpiration" extends="UserCheck"/>
+      <inheritance key="Expiration" class="UserCheck1Expiration" extends="UserCheck1"/>
     </column>
   </table>
 </database>
 XML;
         $this->buildClasses($schema);
-        $this->assertTrue(class_exists('ConstantNameTest1\UserCheckExpiration'));
+        $this->assertTrue(class_exists('ConstantNameTest1\UserCheck1'));
+        $this->assertTrue(class_exists('ConstantNameTest1\UserCheck1Expiration'));
+        $this->assertTrue(class_exists('ConstantNameTest1\Map\UserCheck1TableMap'));
+        $this->assertEquals('Expiration', \ConstantNameTest1\Map\UserCheck1TableMap::CLASSKEY_EXPIRATION);
     }
 
     /**
@@ -50,14 +53,17 @@ XML;
   <table name="radcheck" phpName="UserCheck2">
     <column name="id" type="INTEGER" sqlType="int(11) unsigned" primaryKey="true" autoIncrement="true" required="true"/>
     <column name="attribute" type="VARCHAR" size="64" required="true" inheritance="single">
-      <inheritance key="Calling-Station-Id" class="UserCheckMacAddress" extends="UserCheck2"/>
+      <inheritance key="Calling-Station-Id" class="UserCheck2MacAddress" extends="UserCheck2"/>
     </column>
   </table>
 </database>
 XML;
         $this->buildClasses($schema);
-        $this->assertTrue(class_exists('ConstantNameTest2\Map\UserCheck2Map'));
-        $this->assertEquals('Calling-Station-Id', ConstantNameTest2\Map\UserCheck2Map::CLASSKEY_CALLING_STATION_ID);
+        $this->assertTrue(class_exists('ConstantNameTest2\UserCheck2'));
+        $this->assertTrue(class_exists('ConstantNameTest2\UserCheck2MacAddress'));
+        $this->assertTrue(class_exists('ConstantNameTest2\Map\UserCheck2TableMap'));
+        $this->assertTrue(defined('\ConstantNameTest2\Map\UserCheck2TableMap::CLASSKEY_CALLING_STATION_ID'));
+        $this->assertEquals('Calling-Station-Id', \ConstantNameTest2\Map\UserCheck2TableMap::CLASSKEY_CALLING_STATION_ID);
     }
 
     /**
@@ -71,13 +77,17 @@ XML;
   <table name="radcheck" phpName="UserCheck3">
     <column name="id" type="INTEGER" sqlType="int(11) unsigned" primaryKey="true" autoIncrement="true" required="true"/>
     <column name="attribute" type="VARCHAR" size="64" required="true" inheritance="single">
-      <inheritance key="Key.-_:*" class="UserCheckMacAddress" extends="UserCheck3"/>
+      <inheritance key="Key.-_:*" class="UserCheck3MacAddress" extends="UserCheck3"/>
     </column>
   </table>
 </database>
 XML;
         $this->buildClasses($schema);
-        $this->assertTrue(class_exists('ConstantNameTest3\UserCheckMacAddress'));
+        $this->assertTrue(class_exists('ConstantNameTest3\UserCheck3'));
+        $this->assertTrue(class_exists('ConstantNameTest3\UserCheck3MacAddress'));
+        $this->assertTrue(class_exists('ConstantNameTest3\Map\UserCheck3TableMap'));
+        $this->assertTrue(defined('\ConstantNameTest3\Map\UserCheck3TableMap::CLASSKEY_KEY'));
+        $this->assertEquals('Key.-_:*', \ConstantNameTest3\Map\UserCheck3TableMap::CLASSKEY_KEY);
     }
 
     protected function buildClasses($schema)
