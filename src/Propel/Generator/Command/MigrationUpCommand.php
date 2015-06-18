0fb68c4f9a284a77d3313a821699fab2b0a419b0
Dmitrii Raev <dmitrii.raev@gmail.com>
2015-06-18 17:59:28 +0300

    Update MigrationUpCommand.php
    Don't overwrite propel.php config value with console default one

--- src/Propel/Generator/Command/MigrationUpCommand.php
+++ src/Propel/Generator/Command/MigrationUpCommand.php
@@ -22,8 +22,6 @@ use Propel\Generator\Util\SqlParser;
  */
 class MigrationUpCommand extends AbstractCommand
 {
-    const DEFAULT_MIGRATION_TABLE   = 'propel_migration';
-
     /**
      * {@inheritdoc}
      */
@@ -33,7 +31,7 @@ class MigrationUpCommand extends AbstractCommand
 
         $this
             ->addOption('output-dir',       null, InputOption::VALUE_REQUIRED,  'The output directory')
-            ->addOption('migration-table',  null, InputOption::VALUE_REQUIRED,  'Migration table name', self::DEFAULT_MIGRATION_TABLE)
+            ->addOption('migration-table',  null, InputOption::VALUE_REQUIRED,  'Migration table name')
             ->addOption('connection',       null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Connection to use', array())
             ->addOption('fake',             null, InputOption::VALUE_NONE,  'Does not touch the actual schema, but marks next migration as executed.')
             ->addOption('force',            null, InputOption::VALUE_NONE,  'Continues with the migration even when errors occur.')
@@ -53,7 +51,11 @@ class MigrationUpCommand extends AbstractCommand
         if ($this->hasInputOption('output-dir', $input)) {
             $configOptions['propel']['paths']['migrationDir'] = $input->getOption('output-dir');
         }
-
+        
+        if ($this->hasInputOption('migration-table', $input)) {
+            $configOptions['propel']['migrations']['tableName'] = $input->getOption('migration-table');
+        }
+        
         $generatorConfig = $this->getGeneratorConfig($configOptions, $input);
 
         $this->createDirectory($generatorConfig->getSection('paths')['migrationDir']);
@@ -73,7 +75,7 @@ class MigrationUpCommand extends AbstractCommand
         }
 
         $manager->setConnections($connections);
-        $manager->setMigrationTable($input->getOption('migration-table'));
+        $manager->setMigrationTable($generatorConfig->getSection('migrations')['tableName']);
         $manager->setWorkingDirectory($generatorConfig->getSection('paths')['migrationDir']);
 
         if (!$nextMigrationTimestamp = $manager->getFirstUpMigrationTimestamp()) {
