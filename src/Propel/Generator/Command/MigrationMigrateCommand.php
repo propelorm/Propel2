dcbc2a5402420f34a83c1a0ed0782a37a8450e95
Dmitrii Raev <dmitrii.raev@gmail.com>
2015-06-18 18:01:31 +0300

    Update MigrationMigrateCommand.php
    Don't overwrite propel.php config value with console default one

--- src/Propel/Generator/Command/MigrationMigrateCommand.php
+++ src/Propel/Generator/Command/MigrationMigrateCommand.php
@@ -22,8 +22,6 @@ use Propel\Generator\Util\SqlParser;
  */
 class MigrationMigrateCommand extends AbstractCommand
 {
-    const DEFAULT_MIGRATION_TABLE   = 'propel_migration';
-
     /**
      * {@inheritdoc}
      */
@@ -33,7 +31,7 @@ class MigrationMigrateCommand extends AbstractCommand
 
         $this
             ->addOption('output-dir',       null, InputOption::VALUE_REQUIRED,  'The output directory')
-            ->addOption('migration-table',  null, InputOption::VALUE_REQUIRED,  'Migration table name', self::DEFAULT_MIGRATION_TABLE)
+            ->addOption('migration-table',  null, InputOption::VALUE_REQUIRED,  'Migration table name')
             ->addOption('connection',       null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Connection to use', array())
             ->addOption('fake',             null, InputOption::VALUE_NONE,  'Does not touch the actual schema, but marks all migration as executed.')
             ->addOption('force',            null, InputOption::VALUE_NONE,  'Continues with the migration even when errors occur.')
@@ -54,6 +52,10 @@ class MigrationMigrateCommand extends AbstractCommand
             $configOptions['propel']['paths']['migrationDir'] = $input->getOption('output-dir');
         }
 
+        if ($this->hasInputOption('migration-table', $input)) {
+            $configOptions['propel']['migrations']['tableName'] = $input->getOption('migration-table');
+        }
+        
         $generatorConfig = $this->getGeneratorConfig($configOptions, $input);
 
         $this->createDirectory($generatorConfig->getSection('paths')['migrationDir']);
@@ -73,7 +75,7 @@ class MigrationMigrateCommand extends AbstractCommand
         }
 
         $manager->setConnections($connections);
-        $manager->setMigrationTable($input->getOption('migration-table'));
+        $manager->setMigrationTable($generatorConfig->getSection('migrations')['tableName']);
         $manager->setWorkingDirectory($generatorConfig->getSection('paths')['migrationDir']);
 
         if (!$manager->getFirstUpMigrationTimestamp()) {
