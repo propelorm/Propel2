af2413eed106eeab297087a9abb4bbc295f161fc
nymo <gp@gregorpanek.de>
2015-11-04 06:42:34 +0100

    pager always 0 when no results fixes #1025

--- src/Propel/Runtime/Util/PropelModelPager.php
+++ src/Propel/Runtime/Util/PropelModelPager.php
@@ -263,7 +263,7 @@ class PropelModelPager implements \IteratorAggregate, \Countable
      */
     public function getFirstPage()
     {
-        return 1;
+        return $this->nbResults === 0 ? 0 : 1;
     }
 
     /**
@@ -317,7 +317,7 @@ class PropelModelPager implements \IteratorAggregate, \Countable
     public function setPage($page)
     {
         $this->page = (int) $page;
-        if ($this->page <= 0) {
+        if ($this->page <= 0 && $this->nbResults > 0) {
             // set first page, which depends on a maximum set
             $this->page = $this->getMaxPerPage() ? 1 : 0;
         }
