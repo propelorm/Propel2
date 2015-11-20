9c29762c03b050be1d9b8db0d79c870d81e6bd33
Big_Shark <BigShark666@gmail.com>
2015-11-20 21:27:23 +0700

    Added phpunit .sh files

--- tests/bin/circleci.sh
+++ tests/bin/circleci.sh
@@ -7,22 +7,22 @@ fi
 
 if [ "$CIRCLE_NODE_INDEX" = "0" ]; then
     echo "agnostic tests"
-    ./vendor/bin/phpunit -c tests/agnostic.phpunit.xml --coverage-php=tests/clover.cov";
+    ./vendor/bin/phpunit -c tests/agnostic.phpunit.xml --coverage-php=tests/clover.cov;
 fi
 
 if [ "$CIRCLE_NODE_INDEX" = "1" ]; then
     echo "mysql tests"
-    ./vendor/bin/phpunit -c tests/mysql.phpunit.xml --coverage-php=tests/clover.cov";
+    ./vendor/bin/phpunit -c tests/mysql.phpunit.xml --coverage-php=tests/clover.cov;
 fi
 
 if [ "$CIRCLE_NODE_INDEX" = "2" ]; then
     echo "postgresql tests"
-    ./vendor/bin/phpunit -c tests/pgsql.phpunit.xml --coverage-php=tests/clover.cov";
+    ./vendor/bin/phpunit -c tests/pgsql.phpunit.xml --coverage-php=tests/clover.cov;
 fi
 
 if [ "$CIRCLE_NODE_INDEX" = "3" ]; then
     echo "sqlite tests"
-    ./vendor/bin/phpunit  -c tests/sqlite.phpunit.xml --coverage-php=tests/clover.cov";
+    ./vendor/bin/phpunit -c tests/sqlite.phpunit.xml --coverage-php=tests/clover.cov;
 fi
 
 if [ "$PHPUNIT_COVERAGE" = "1" ]; then
