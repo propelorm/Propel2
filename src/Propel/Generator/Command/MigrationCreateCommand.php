278cdcf590e2d5d6077c026b43bc67d869e66a5a
Fredrik Wollsén <fredrik@neam.se>
2015-12-05 15:13:11 +0200

    Added migration create command for creating empty migration classes

---
+++ src/Propel/Generator/Command/MigrationCreateCommand.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Propel\Generator\Command;
+
+use Propel\Common\Config\ConfigurationManager;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+use Propel\Generator\Exception\RuntimeException;
+use Propel\Generator\Manager\MigrationManager;
+
+/**
+ * @author William Durand <william.durand1@gmail.com>
+ * @author Fredrik Wollsén <fredrik@neam.se>
+ */
+class MigrationCreateCommand extends AbstractCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->addOption('schema-dir',         null, InputOption::VALUE_REQUIRED,  'The directory where the schema files are placed')
+            ->addOption('output-dir',         null, InputOption::VALUE_REQUIRED,  'The output directory where the migration files are located')
+            ->addOption('connection',         null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Connection to use. Example: \'bookstore=mysql:host=127.0.0.1;dbname=test;user=root;password=foobar\' where "bookstore" is your propel database name (used in your schema.xml)', [])
+            ->addOption('editor',             null, InputOption::VALUE_OPTIONAL,  'The text editor to use to open diff files', null)
+            ->addOption('comment',            "m",  InputOption::VALUE_OPTIONAL,  'A comment for the migration', '')
+            ->setName('migration:create')
+            ->setDescription('Create an empty migration class')
+            ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $configOptions = [];
+
+        if ($this->hasInputOption('connection', $input)) {
+            foreach ($input->getOption('connection') as $conn) {
+                $configOptions += $this->connectionToProperties($conn);
+            }
+        }
+
+        if ($this->hasInputOption('schema-dir', $input)) {
+            $configOptions['propel']['paths']['schemaDir'] = $input->getOption('schema-dir');
+        }
+
+        if ($this->hasInputOption('output-dir', $input)) {
+            $configOptions['propel']['paths']['migrationDir'] = $input->getOption('output-dir');
+        }
+
+        $generatorConfig = $this->getGeneratorConfig($configOptions, $input);
+
+        $this->createDirectory($generatorConfig->getSection('paths')['migrationDir']);
+
+        $manager = new MigrationManager();
+        $manager->setGeneratorConfig($generatorConfig);
+        $manager->setSchemas($this->getSchemas($generatorConfig->getSection('paths')['schemaDir'], $input->getOption('recursive')));
+
+        $migrationsUp   = [];
+        $migrationsDown = [];
+        foreach ($manager->getDatabases() as $appDatabase) {
+            $name = $appDatabase->getName();
+            $migrationsUp[$name]    = '';
+            $migrationsDown[$name]  = '';
+        }
+
+        $timestamp = time();
+        $migrationFileName  = $manager->getMigrationFileName($timestamp);
+        $migrationClassBody = $manager->getMigrationClassBody($migrationsUp, $migrationsDown, $timestamp, $input->getOption('comment'));
+
+        $file = $generatorConfig->getSection('paths')['migrationDir'] . DIRECTORY_SEPARATOR . $migrationFileName;
+        file_put_contents($file, $migrationClassBody);
+
+        $output->writeln(sprintf('"%s" file successfully created.', $file));
+
+        if (null !== $editorCmd = $input->getOption('editor')) {
+            $output->writeln(sprintf('Using "%s" as text editor', $editorCmd));
+            shell_exec($editorCmd . ' ' . escapeshellarg($file));
+        } else {
+            $output->writeln('Now add SQL statements and data migration code as necessary.');
+            $output->writeln('Once the migration class is valid, call the "migrate" task to execute it.');
+        }
+    }
+
+}
