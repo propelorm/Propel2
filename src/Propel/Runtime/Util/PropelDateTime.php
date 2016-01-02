5caf7cde2a4cde8425d87c6ba3c6cfd7f163ce0b
Jeremy Denoun <jeremy.denoun@iguanesolutions.com>
2016-01-02 19:53:14 +0100

    proposal for add fallback on a fake DateTime object if value contain % symbol for LIKE query condition

--- src/Propel/Runtime/Util/PropelDateTime.php
+++ src/Propel/Runtime/Util/PropelDateTime.php
@@ -13,6 +13,19 @@ namespace Propel\Runtime\Util;
 use \DateTimeZone;
 use Propel\Runtime\Exception\PropelException;
 
+class FakeDateTime {
+    private $time;
+
+    function __construct($time = "now", $tz = null)
+    {
+        $this->time = $time;
+    }
+
+    function format($format = "") {
+        return $this->time;
+    }
+}
+
 /**
  * DateTime subclass which supports serialization.
  *
@@ -85,6 +98,10 @@ class PropelDateTime extends \DateTime
                 // because of a DateTime bug: http://bugs.php.net/bug.php?id=43003
                 $dateTimeObject->setTimeZone(new \DateTimeZone(date_default_timezone_get()));
             } else {
+                if (strpos($value, "%") !== false) {
+                    return new FakeDateTime($value, $timeZone);
+                }
+
                 if (null === $timeZone) {
                     // stupid DateTime constructor signature
                     $dateTimeObject = new $dateTimeClass($value);
