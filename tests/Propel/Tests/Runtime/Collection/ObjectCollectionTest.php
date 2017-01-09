459534053bc9a11a5f8e4b52ab50c15371fb8a15
Peter Potrowl <peter.potrowl@gmail.com>
2017-01-09 15:53:51 +0100

    Fix regression in ObjectCollection of Collections.
    Was failing with: `Propel\Runtime\Exception\BadMethodCallException: Call to undefined
    method: hashCode` because Collection.php has a __call method and no
    hashCode method.

--- tests/Propel/Tests/Runtime/Collection/ObjectCollectionTest.php
+++ tests/Propel/Tests/Runtime/Collection/ObjectCollectionTest.php
@@ -266,6 +266,20 @@ class ObjectCollectionTest extends BookstoreTestBase
         $this->assertFalse(0 === $col->search($b2));
     }
 
+    public function testObjectCollectionOfObjectCollections()
+    {
+        $col1 = new ObjectCollection();
+        $b1  = new Book();
+        $b1->setTitle('Bar');
+        $b1->setISBN('012345');
+        $col2  = clone $b1;
+
+        $col = new ObjectCollection([$col1]);
+
+        $this->assertTrue($col->contains($col1));
+        $this->assertFalse($col->contains($col2));
+    }
+
     /**
      * @afterClass
      */
