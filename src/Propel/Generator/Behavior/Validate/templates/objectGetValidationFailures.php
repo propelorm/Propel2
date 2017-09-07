66b4a57e98a45bafae698849ce9ef08f994eb700
Gaiidenn <j.vanbersy@gmail.com>
2017-09-07 12:06:09 +0200

    Update objectGetValidationFailures.php
    Updated comment for better IDE interpretations and auto-completion

--- src/Propel/Generator/Behavior/Validate/templates/objectGetValidationFailures.php
+++ src/Propel/Generator/Behavior/Validate/templates/objectGetValidationFailures.php
@@ -3,7 +3,7 @@
  * Gets any ConstraintViolation objects that resulted from last call to validate().
  *
  *
- * @return     object ConstraintViolationList
+ * @return     object|ConstraintViolationList
  * @see        validate()
  */
 public function getValidationFailures()
