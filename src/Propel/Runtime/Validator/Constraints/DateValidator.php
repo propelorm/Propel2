94b9e813525c0f41a2e65905285cf56aadc40b22
Brian Graham <Incognito@users.noreply.github.com>
2020-07-07 11:31:00 +0200

    Update src/Propel/Runtime/Validator/Constraints/DateValidator.php
    Co-authored-by: Denis Turkov <denis.turkov@spryker.com>

--- src/Propel/Runtime/Validator/Constraints/DateValidator.php
+++ src/Propel/Runtime/Validator/Constraints/DateValidator.php
@@ -14,7 +14,8 @@ use Symfony\Component\Validator\Constraint;
 
 /**
  * Validates Dates
- * Supports \DateTimeInterface values which were removed from the DateValidator in Symfony 5.
+ * Provides FC on \DateTimeInterface values which were removed from the DateValidator in Symfony 5.
+ * Propel expectations on DateTime validation are detaching starting with Symfony 5.
  */
 class DateValidator extends SymfonyDateValidator
 {
