4b7c3f72a57542c0d0ce91e779070d058fef28be
mscherer <mark.scherer@spryker.com>
2020-07-29 10:00:36 +0200

    Fix the license header and add sniff rule.

--- src/Propel/Generator/Command/Helper/ConsoleHelper3.php
+++ src/Propel/Generator/Command/Helper/ConsoleHelper3.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * MIT License. This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Propel\Generator\Command\Helper;
 
 use Symfony\Component\Console\Helper\QuestionHelper;
