8cf7fe5a5c7986ddc473b88ceb43379fa2067da5
Bezpiatov <aleksandr.bezpiatov@spryker.com>
2020-10-23 10:39:52 +0300

    Fixed method signatures

--- src/Propel/Runtime/Adapter/SqlAdapterInterface.php
+++ src/Propel/Runtime/Adapter/SqlAdapterInterface.php
@@ -8,7 +8,7 @@
 
 namespace Propel\Runtime\Adapter;
 
-use Propel\Runtime\Connection\StatementWrapper;
+use Propel\Runtime\Connection\StatementInterface;
 use Propel\Runtime\ActiveQuery\Criteria;
 use Propel\Runtime\Map\ColumnMap;
 use Propel\Runtime\Map\DatabaseMap;
@@ -118,19 +118,19 @@ interface SqlAdapterInterface extends AdapterInterface
      * $stmt->execute();
      * </code>
      *
-     * @param StatementWrapper $stmt
+     * @param \Propel\Runtime\Connection\StatementInterface $stmt
      * @param array $params array('column' => ..., 'table' => ..., 'value' => ...)
      * @param \Propel\Runtime\Map\DatabaseMap $dbMap
-     * 
+     *
      * @return void
      */
-    public function bindValues(StatementWrapper $stmt, array $params, DatabaseMap $dbMap);
+    public function bindValues(StatementInterface $stmt, array $params, DatabaseMap $dbMap);
 
     /**
      * Binds a value to a positioned parameter in a statement,
      * given a ColumnMap object to infer the binding type.
      *
-     * @param StatementWrapper $stmt The statement to bind
+     * @param \Propel\Runtime\Connection\StatementInterface $stmt The statement to bind
      * @param string $parameter Parameter identifier
      * @param mixed $value The value to bind
      * @param \Propel\Runtime\Map\ColumnMap $cMap The ColumnMap of the column to bind
@@ -138,5 +138,5 @@ interface SqlAdapterInterface extends AdapterInterface
      *
      * @return bool
      */
-    public function bindValue(StatementWrapper $stmt, $parameter, $value, ColumnMap $cMap, $position = null);
+    public function bindValue(StatementInterface $stmt, $parameter, $value, ColumnMap $cMap, $position = null);
 }
