cf62a9c2ffaf672fbd1c37e5e5f96576f4208caa
Djairho Geuens <djairho@systenamics.be>
2021-06-13 16:04:25 +0200

    Revert "Revert "Adds missing condition""
    This reverts commit e3d8a7cbf9a111c6b5a84581f1d3cb109dd08f9f.

--- src/Propel/Generator/Reverse/MysqlSchemaParser.php
+++ src/Propel/Generator/Reverse/MysqlSchemaParser.php
@@ -419,7 +419,7 @@ EOT;
                     // split foreign key information -> search for ON DELETE and afterwards for ON UPDATE action
                     foreach (array_keys($fkactions) as $fkaction) {
                         $result = null;
-                        preg_match('/' . $fkaction . ' (' . ForeignKey::CASCADE . '|' . ForeignKey::SETNULL . '|' . ForeignKey::RESTRICT . ')/', $fkey, $result);
+                        preg_match('/' . $fkaction . ' (' . ForeignKey::CASCADE . '|' . ForeignKey::SETNULL . '|' . ForeignKey::RESTRICT . '|' . ForeignKey::NOACTION . ')/', $fkey, $result);
                         if ($result && is_array($result) && isset($result[1])) {
                             $fkactions[$fkaction] = $result[1];
                         }
