308165c7b0151a1ca04a1f007684eff5d52316fe
Gregor Harlan <mail@gh01.de>
2021-07-14 16:24:58 +0200

    cs

--- src/Propel/Common/Config/PropelConfiguration.php
+++ src/Propel/Common/Config/PropelConfiguration.php
@@ -29,7 +29,7 @@ class PropelConfiguration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('propel');
 
-        if (\method_exists($treeBuilder, 'getRootNode')) {
+        if (method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->getRootNode();
         } else {
             // BC layer for symfony/config 4.1 and older
