ace340ea5e380dc1fa9fe32ff1cb31b3c363687a
Fabien Bourigault <fbourigault@users.noreply.github.com>
2022-01-04 10:05:51 +0100

    Remove Symfony <4.4 support

--- src/Propel/Common/Config/PropelConfiguration.php
+++ src/Propel/Common/Config/PropelConfiguration.php
@@ -29,12 +29,8 @@ class PropelConfiguration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('propel');
 
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('propel');
-        }
+        /** @var \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
 
         $this->addGeneralSection($rootNode);
         $this->addExcludeTablesSection($rootNode);
