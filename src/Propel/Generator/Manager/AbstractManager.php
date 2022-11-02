7c3f30c3ccef633bc3abe596d9ad000cfdf5f39b
mringler <m@mringler.net>
2022-11-02 13:12:45 +0100

    psalm-fix

--- src/Propel/Generator/Manager/AbstractManager.php
+++ src/Propel/Generator/Manager/AbstractManager.php
@@ -17,7 +17,7 @@ use Propel\Generator\Exception\BuildException;
 use Propel\Generator\Exception\EngineException;
 use Propel\Generator\Model\Database;
 use Propel\Generator\Model\Schema;
-use XsltProcessor;
+use XSLTProcessor;
 
 /**
  * An abstract base Propel manager to perform work related to the XML schema
@@ -323,7 +323,7 @@ abstract class AbstractManager
                     // normalize the document using normalizer stylesheet
                     $xslDom = new DOMDocument('1.0', 'UTF-8');
                     $xslDom->load($this->xsl->getAbsolutePath());
-                    $xsl = new XsltProcessor();
+                    $xsl = new XSLTProcessor();
                     $xsl->importStyleSheet($xslDom);
                     $dom = $xsl->transformToDoc($dom);
                 }
