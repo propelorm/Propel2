43dfc68d588268f27eba1cec66039629c35ae37a
Rizwan Jiwan <33099935+rizwanjiwan@users.noreply.github.com>
2024-09-17 13:54:00 -0400

    Check if matches are empty strings, not if they exist
    Fix phpstan error.

--- src/Propel/Generator/Reverse/MysqlSchemaParser.php
+++ src/Propel/Generator/Reverse/MysqlSchemaParser.php
@@ -237,7 +237,7 @@ class MysqlSchemaParser extends AbstractSchemaParser
         $/x';
         if (preg_match($regexp, $row['Type'], $matches)) {
             $nativeType = $matches[1];
-            if ($matches[2]) {
+            if (empty($matches[2])===false) {
                 $cpos = strpos($matches[2], ',');
                 if ($cpos !== false) {
                     $size = (int)substr($matches[2], 0, $cpos);
@@ -246,7 +246,7 @@ class MysqlSchemaParser extends AbstractSchemaParser
                     $size = (int)$matches[2];
                 }
             }
-            if ($matches[3]) {
+            if (empty($matches[3])===false) {
                 $sqlType = $row['Type'];
             }
             if (isset(static::$defaultTypeSizes[$nativeType]) && $scale == null && $size === static::$defaultTypeSizes[$nativeType]) {
