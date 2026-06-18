31b8d81c00a54306a60f89e75483061142474c74
gechetspr <serhii.chepela@spryker.com>
2026-06-18 13:24:45 +0300

    Reverted old commit that change cross reff behaviour

--- src/Propel/Generator/Model/ForeignKey.php
+++ src/Propel/Generator/Model/ForeignKey.php
@@ -874,7 +874,7 @@ class ForeignKey extends MappingModel
      */
     public function isAtLeastOneLocalPrimaryKeyIsRequired(): bool
     {
-        foreach ($this->getLocalColumnObjects() as $pk) {
+        foreach ($this->getLocalPrimaryKeys() as $pk) {
             if ($pk->isNotNull() && !$pk->hasDefaultValue()) {
                 return true;
             }
